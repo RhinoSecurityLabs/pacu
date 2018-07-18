3bcd3ed0676995298358709ba187b6b4d17548f1
Alex Morgenstern <alexander.morgenstern@rhinosecuritylabs.com>
2018-07-18 14:58:02 -0700

    Added comments and made formatting changes

--- modules/enum_permissions_bruteforce/main.py
+++ modules/enum_permissions_bruteforce/main.py
@@ -1,25 +1,12 @@
 #!/usr/bin/env python3
 import argparse
 import boto3
-import botocore
 from botocore.exceptions import ClientError
 from botocore.exceptions import ParamValidationError
 from botocore.exceptions import UnknownServiceError
-from botocore.exceptions import EndpointConnectionError
-from botocore.exceptions import NoRegionError
 import os
-import urllib
-
-from copy import deepcopy
-
-from .region_parse import RegionParser
-
-import re
+from datetime import datetime
 import json
-
-
-# When writing a module, feel free to remove any comments, placeholders, or
-# anything else that doesn't relate to your module.
 
 module_info = {
     'name': 'enum_permissions_bruteforce',
@@ -29,7 +16,7 @@ module_info = {
     'description':
         """
         This module will automatically run through all possible
-        API calls in order to enumerate permissions without the use of 
+        API calls in order to enumerate permissions without the use of
         IAM permissions
         """,
     'services': ['ALL'],
@@ -47,85 +34,10 @@ parser.add_argument(
     help='A comma separated list of services to brute force permissions'
 )
 
-module_dir = os.path.dirname(__file__)
-path = os.path.join(module_dir, 'special_params.json')
-data = open(path).read()
-special_types = json.loads(data)
-
-region_parser = RegionParser()
-complete_regions = region_parser.parse()
-
-service_name_mismatch_mapper = {
-    'application-autoscaling':'autoscaling',
-    'appstream':'aas2',
-    'autoscaling-plans':'as',
-    'budgets':'billing-budgets',
-    'ce':'billing-ce',
-    'cloudformation':'cfn',
-    'cloudfront':'any',
-    'cloudhsmv2':'cloudhsm',
-    'cloudsearchdomain':'cloudsearch',
-    'cloudtrail':'ct',
-    'cloudwatch':'cw',
-    'cognito-identity':'cognito_identity_federated_identities',
-    'cognito-idp':'cognito_identity_your_user_pools',
-    'cognito-sync':'cognito_sync',
-    'config':'awsconfig',
-    'cur':'billing-ce',
-    'dax':'ddb_dax',
-    'directconnect':'dc',
-    'dynamodb':'ddb',
-    'dynamodbstreams':'ddb_streams',
-    'efs':'elasticfilesystem-region',
-    'elbv2':'elb',
-    'es':'elasticsearch-service-regions',
-    'events':'cwe',
-    'firehose':'fh',
-    'fms':'firewallmanager',
-    'health':'default',
-    #'importexport':'fixme',
-    'iot-data':'iot',
-    'iot-jobs-data':'iot',
-    'kinesis':'ak',
-    'kinesis-video-archived-media':'akv',
-    'kinesis-video-media':'akv',
-    'kinesisanalytics':'ka',
-    'kinesisvideo':'akv',
-    'lex-models':'lex',
-    'lex-runtime':'lex',
-    'logs':'cwl',
-    'marketplace-entitlement':'default',
-    'marketplacecommerceanalytics':'default',
-    'mediastore-data':'mediastore',
-    'meteringmarketplace':'default',
-    'mgh':'migrationhub-region',
-    'mobile':'mobile_analytics',
-    'mq':'amazon-mq',
-    'mturk':'amt',
-    'opsworkscm':'opsworks-for-chef-automate',
-    'organizations':'ao',
-    'pi':'all',
-    'polly':'pol',
-    'pricing':'billing-pricing',
-    'resource-groups':'arg',
-    'resourcegroupstaggingapi':'arg',
-    'route53':'r53',
-    'route53domains':'r53',
-    'sagemaker-runtime':'sagemaker',
-    'secretsmanager':'asm',
-    'servicediscovery':'r53',
-    'sms':'server_migration',
-    'stepfunctions':'step-functions',
-    'storagegateway':'sg',
-    'support':'default',
-    'waf-regional':'waf',
-    'workmail':'wm',
-    'workspaces':'wsp'
-}
-
 allow_permissions = {}
 deny_permissions = {}
-maybe_permissions = {}
+possible_permissions = {}
+bugged_permissions = []
 
 def help():
     return [module_info, parser.format_help()]
@@ -134,20 +46,17 @@ def help():
 def build_service_list():
     try:
         client = boto3.client(
-            'bad_service_name',
+            'bad_dummy_service_name',
             region_name='us-east-1'
         )
+        return []
     except UnknownServiceError as error:
         service_string = str(error)[62:]
         return service_string.split(', ').copy()
 
 
-def get_regions(service):
-    if service in service_name_mismatch_mapper:
-        service = service_name_mismatch_mapper[service]
-    return complete_regions[service]
-
 def missing_param(param):
+    """Return a key/value dict in the expected formatting given a common parameter, or defaults to param=>'dummy_data'."""
     common_params = {
         'Storage': {
             'S3': {
@@ -173,42 +82,86 @@ def missing_param(param):
         'VpnConnectionId': 'vpn-83ad48ea',
         'CustomerGatewayId': 'cgw-0e11f160',
         'InputStorageLocation': {'Bucket': 'key'},
-        'LaunchTemplateData': {'ImageId':'ami-aabbccdd'},
+        'LaunchTemplateData': {'ImageId': 'ami-aabbccdd'},
         'PeerVpcId': 'vpc-abcd1235',
-        'InternetGatewayId': 'igw-12312312'
+        'InternetGatewayId': 'igw-12312312',
+        'NetworkAclId': 'acl-123123123',
+        'Attribute': 'productCodes',
+        'Recurrence': {'Frequency': 'string'},
+        'FirstSlotStartTimeRange': {'EarliestTime': datetime(2015, 1, 1), 'LatestTime': datetime(2015, 1, 1)},
+        'Volume': {'Size': 123},
+        'Image': {'Bytes': 123, 'Format': 'RAW', 'ImportManifestUrl': 'string'},
+        'InstanceCreditSpecifications': [{'InstanceId': 'val'}],
+        'InstanceIds': ['i-abc123'],
+        'HostIdSet': ['idset'],
+        'PurchaseRequests': [{'InstanceCount': 5, 'PurchaseToken': 'abc'}],
+        'VpcEndpointIds': ['vpcids'],
+        'HostIds': ['123'],
+        'Status': 'ok',
+        'ReasonCodes': ['unresponsive'],
+        'Instances': ['i-abc123'],
+        'SpotFleetRequestConfig': {'IamFleetRole': 'test', 'TargetCapacity': 1},
+        'ImageId': 'test',
+        'LaunchSpecification': {'ImageId': 'i-123'},
+        'Ipv6Addresses': ['aa:aa:aa:aa:aa:aa'],
+        'PrivateIpAddresses': ['1.1.1.1'],
+        'IpPermissions': [{'FromPort': 1}],
+        'SpotFleetRequestIds': ['test'],
+        'SpotInstanceRequestIds': ['test'],
+        'ResourceIds': ['test'],
+        'ConnectionEvents': ['test'],
+        'IamInstanceProfile': {'Name': 'test'},
+        'ReservedInstancesIds': ['dummydata'],
+        'TargetConfigurations': [{'AvailabilityZone': 'dummydata'}]
 
     }
-    out = {param: common_params[param]} if param in common_params else {param: 'DryRun'} 
+    out = {param: common_params[param]} if param in common_params else {param: 'dummy_data'}
     return out
 
+
 def invalid_param(valid_type):
+    """Returns an object matching the requested valid type."""
     print('Checking for invalid types')
     types = {
-        'list': ['DryRun',],
+        'datetime.datetime': datetime(2015, 1, 1),
+        'list': ['test'],
         'int': 1,
         'dict': {'DryRun': True},
         'bool': True
     }
-    
     return types[valid_type]
 
+
 def error_delegator(error):
+    """Processes the complete error message. Trims the error response to not overwrite missing data with a valid type error"""
     kwargs = {}
     # Ignore first line of error message and process in reverse order.
     for line in str(error).split('\n')[::-1][:-1]:
-        print(f'\t Processing Line: {line}')
+        print('    Processing Line: {}'.format(line))
         if 'Missing required parameter' in line:
             if line[line.find('"') + 1:-1] not in kwargs.keys():
                 kwargs = {**kwargs, **missing_param(line.split()[-1][1:-1])}
-        if 'Invalid type for parameter' in line:            
+        if 'Invalid type for parameter' in line:
             param_name = line.split()[4][:-1]
-
-            valid_type = line.split("'")[3]
-            kwargs[param_name] = invalid_param(valid_type)
+            # Convert list of strings to list of dicts of invalid list subtype found.
+            if param_name[:-3] == '[0]':
+                kwargs[param_name] = [{'DryRun': True}]
+            else:
+                valid_type = line.split("'")[3]
+                kwargs[param_name] = invalid_param(valid_type)
     return kwargs
 
 
+def generate_preload_actions():
+    """Certain actions require parameters that cannot be easily discerned from the error message provided by preloading kwargs for those actions."""
+    module_dir = os.path.dirname(__file__)
+    path = os.path.join(module_dir, 'preload_actions.json')
+    data = open(path).read()
+    return json.loads(data)
+
+
 def valid_func(func):
+    """Returns False for service functions that don't correspond to an AWS API action"""
     if func[0] == '_':
         return False
     bad_functions = [
@@ -219,35 +172,45 @@ def valid_func(func):
         'generate_presigned_url',
         'exceptions',
         'meta',
-        # EC2
-        'accept_vpc_peering_connection', # Needs valid vpc peering connection ID
-        'associate_address', # Needs valid public IP or allocation id
-        'associate_iam_instance_profile', # Needs valid instance ID
-        'attach_classic_link_vpc', # Needs valid instance ID
-        'attach_volume', # Requires valid volume ID
-        'authorize_security_group_egress', # Requires valid security group
-        'authorize_security_group_ingress', # Requires valid security group
-        'create_image', # Requires valid instance ID
-        'create_launch_template_version', # Requires valid template ID
-        'create_network_interface_permission', # Requires valid interface ID
-        'create_snapshot', # Requires valid volume ID
-        'create_tags', # Requiresvalid image ID
-        'create_volume', # Need to pass current regions availability zone
-        'create_vpc_peering_connection', # Requires valid vpc
-        'delete_customer_gateway', # Requires valid customer gateway
-        'delete_dhcp_options', # Requires valid DhcpOptionsId
-        'delete_launch_template', # Requires valid template ID
     ]
     if func in bad_functions:
         return False
     return True
 
+
+def convert_special_params(func, kwargs):
+    """Certain actions go through additional argument parsing. If such a case exists, the dummy_data will
+    be filled with valid data so that the action can successfully pass validation and reach and query
+    correctly determine authorization.
+
+    TODO: Go through and actually make sure that when a parameter is requested, it can be programatically
+    returned. If you need a valid InstanceID and you have the permission to get one, you should be able to
+    fill that valid data and determine authorization.
+    
+    """
+    special_params = {
+        'reset_image_attribute': {'Attribute': 'launchPermission'},
+        'reset_instance_attribute': {'Attribute': 'kernel'},
+        'reset_snapshot_attribute': {'Attribute': 'createVolumePermission'},
+    }
+    if func in special_params:
+        print('    Matching Function Found')
+        for param in special_params[func]:
+            kwargs[param] = special_params[func][param]
+        print('    Replaced Parameters')
+        return True
+    else:
+        print('    No special paramaters found for function: {}'.format(func))
+        return False
+
+
 def main(args, pacu_main):
     session = pacu_main.get_active_session()
     args = parser.parse_args(args)
     print = pacu_main.print
-    
-    index = 54
+    get_regions = pacu_main.get_regions
+
+    preload_actions = generate_preload_actions()
 
     service_list = []
     if args.services:
@@ -255,38 +218,32 @@ def main(args, pacu_main):
     else:
         service_list = build_service_list()
 
-
     for service in service_list:
         allow_permissions[service] = []
         deny_permissions[service] = []
-        maybe_permissions[service] = []
-        regions = ['none']
-        try:
-            client = boto3.client(
-                service
-            )
-        except NoRegionError:
-            regions = get_regions(service)
+        possible_permissions[service] = []
 
+        regions = get_regions(service)
         regions = ['us-east-1']
         for region in regions:
             client = boto3.client(
                 service,
-                region_name = region,
+                region_name=region,
             )
             functions = [func for func in dir(client) if valid_func(func)]
-            for func in functions[index:]:
-                print('*************************NEW FUNCTION({})*************************'.format(index))
+            index = 1
+            for func in functions:
+                print('*************************NEW FUNCTION({}/{})*************************'.format(index, len(functions)))
                 index += 1
-                kwargs = special_types[func] if func in special_types else {}
+                kwargs = preload_actions[func] if func in preload_actions else {}
                 kwargs['DryRun'] = True
                 while True:
-                    try:                        
+                    try:
                         print('---------------------------------------------------------')
-                        print(f'Trying {func}...')
-                        print(f'Kwargs: {kwargs}')
+                        print('Trying {}...'.format(func))
+                        print('Kwargs: {}'.format(kwargs))
                         caller = getattr(client, func)
-                        result = caller(**kwargs)
+                        caller(**kwargs)
                         allow_permissions[service].append(func)
                         print('Authorization exists for: {}'.format(func))
                         break
@@ -295,35 +252,46 @@ def main(args, pacu_main):
                             print('DryRun failed. Retrying without DryRun parameter')
                             del kwargs['DryRun']
                         else:
-                            kwargs = {**kwargs, **error_delegator(error)}
+                            if 'AvailabilityZone' not in kwargs and 'AvailabilityZone' in str(error):
+                                print("Adding Availability Zone")
+                                kwargs['AvailabilityZone'] = region + 'a'
+                            else:
+                                print('Parameter Validation Error: {}'.format(error))
+                                kwargs = {**kwargs, **error_delegator(error)}
                     except ClientError as error:
+                        # DryRun returned true, adding to allowed permissions
                         if error.response['Error']['Code'] == 'DryRunOperation':
                             allow_permissions[service].append(func)
                             print('Authorization exists for: {}'.format(func))
                             break
 
-                        print(f'ClientError: {error}')
+                        # Error with request raised.
+                        print('ClientError: {}'.format(error))
                         code = error.response['Error']['Code']
-                        if code == 'AccessDeniedException' or 'UnauthorizedOperation' in str(error):
-                            print(f'Unauthorized for permission: {service}:{func}')
+                        if code == 'AccessDeniedException' or code == 'OptInRequired' or 'Unauthorized' in str(error):
+                            print('Unauthorized for permission: {}:{}'.format(service, func))
                             deny_permissions[service].append(func)
                             break
                         elif code == 'MissingParameter':
                             param = str(error).split()[-1]
                             param = param[0].upper() + param[1:]
                             kwargs = {**kwargs, **missing_param(param)}
-                        #elif 'Malformed' in error.response['Error']['Code']:
-                        #    print('Unable to determine authorization')
-                        #    maybe_permissions[service].append(func)
-                        #    break
+                        # If action is not supported, skip.
                         elif code == 'UnsupportedOperation':
                             break
+                        elif code == 'InvalidRequest' or code == 'InvalidParameterValue' or 'Malformed' in code or 'NotFound' in code or 'Unknown' in code:
+                            print('Special Parameter Found')
+                            if not convert_special_params(func, kwargs):
+                                print('No suitable valid data could be found')
+                                possible_permissions[service].append(func)
+                                break
+
                         else:
                             print('Unknown error:')
                             print(error)
-                            maybe_permissions[service].append(func)                            
+                            bugged_permissions.append('{}:{}'.format(service, func))
                             print('*************************END FUNCTION*************************\n')
-                            return
+                            break
             break
         break
 
@@ -333,29 +301,11 @@ def main(args, pacu_main):
     print('Denied Permissions: ')
     print(deny_permissions)
 
-    print('Maybe Permissions: ')
-    print(maybe_permissions)
+    print('Possible Permissions: ')
+    print(possible_permissions)
 
-
-
-
-
-            
-        
-
-    #for region in regions:
-    #    print('Starting region {}...'.format(region))
-    #    client = boto3.client(
-    #        'aws_service',
-    #        region_name=region,
-    #        aws_access_key_id=session.access_key_id,
-    #        aws_secret_access_key=session.secret_access_key,
-    #        # Even if the session doesn't have a session token, this will work
-    #        # because the value will be None and will be ignored.
-    #        aws_session_token=session.session_token,
-    #        # Proxy boto3's client if currently proxying through an agent:
-    #        config=botocore.config.Config(proxies={'https': 'socks5://127.0.0.1:8001', 'http': 'socks5://127.0.0.1:8001'}) if not proxy_settings.target_agent == [] else None
-    #    )
+    print('Bugged Actions')
+    print(bugged_permissions)
 
     print(f"{module_info['name']} completed.\n")
     return

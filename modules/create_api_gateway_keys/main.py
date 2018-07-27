e7504d1c8609accef6664f2e18a7b03bfe61753d
Alex Morgenstern <alexander.morgenstern@rhinosecuritylabs.com>
2018-07-27 16:12:54 -0700

    Added key cleanup function

--- modules/create_api_gateway_keys/main.py
+++ modules/create_api_gateway_keys/main.py
@@ -33,28 +33,68 @@ module_info = {
 
 parser = argparse.ArgumentParser(add_help=False, description=module_info['description'])
 parser.add_argument('--regions', required=False, default=None, help='One or more (comma separated) AWS regions in the format us-east-1. Defaults to all session regions.')
+parser.add_argument('--cleanup', required=False, default=None, action='store_true', help='Searches for Pacu keys previously generated and removes them.')
+
+
+def cleanup(pacu_main, regions):
+    for region in regions:
+        client = pacu_main.get_boto3_client('apigateway', region)
+        try:
+            keys = client.get_api_keys()['items']
+            if len(keys) < 1:
+                print('  No keys were found for {}'.format(region))
+            for key in keys:
+                if key['name'] == 'Pacu':
+                    try:
+                        client.delete_api_key(apiKey=key['id'])
+                        print('  Key deletion successul for: {}'.format(region))
+                    except ClientError as error:
+                        if error.response['Error']['Code'] == 'AccessDeniedException':
+                            print('The current credentials lacks the authorization to delete API keys.')
+                            return False
+        except ClientError as error:
+            if error.response['Error']['Code'] == 'AccessDeniedException':
+                print('The current credentials lacks the authorization to list API keys.')
+                return False
+    return True
+
 
 def main(args, pacu_main):
     session = pacu_main.get_active_session()
-
-    ###### Don't modify these. They can be removed if you are not using the function.
     args = parser.parse_args(args)
     print = pacu_main.print
     get_regions = pacu_main.get_regions
-    ######
-
     regions = args.regions.split(',') if args.regions else get_regions('apigateway')
-
-    lambda_data = {}
     summary_data = {}
-    lambda_data['Functions'] = []
+
+    if args.cleanup:
+        if cleanup(pacu_main, regions):
+            print('  Successfully cleaned up old Pacu keys.')
+            summary_data['cleanup'] = True
+        else:
+            print('  Keys were not successfully cleaned up.')
+            summary_data['cleanup'] = False
+        user_input = input('  Do you want to continue key creation? (y/n)')
+        if user_input != 'y':
+            return summary_data
+
     for region in regions:
         print('Starting region {}...'.format(region))
         client = pacu_main.get_boto3_client('apigateway', region)
-        response = client.create_api_key()
+        try:
+            response = client.create_api_key(name='Pacu')
+            print('  Successfully created API key for: {}'.format(region))
+        except ClientError as error:
+            if error.response['Error']['Code'] == 'AccessDeniedException':
+                print('Unable to add key due to lack of authorization')
     return summary_data
 
 
 def summary(data, pacu_main):
     out = ''
+    if 'cleanup' in data:
+        if data['cleanup']:
+            out += '  Old keys were successfully removed\n'
+        else:
+            out += '  Old keys were not successfully removed\n'
     return out

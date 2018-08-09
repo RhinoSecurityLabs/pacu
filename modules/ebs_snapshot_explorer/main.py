fe491e4f6fa677a89e411750e248c2809c14e0a7
Alex Morgenstern <alexander.morgenstern@rhinosecuritylabs.com>
2018-08-09 13:34:40 -0700

    Added availability zone fetching

--- modules/ebs_snapshot_explorer/main.py
+++ modules/ebs_snapshot_explorer/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Module for ebs_snapshot_explorer"""
 
-#from botocore.exceptions import ClientError
+# from botocore.exceptions import ClientError
 from . import parser
 from . import module_info
 
@@ -13,32 +13,51 @@ def get_interactive_instance(pacu_main, session, region):
     Returns:
         str: The instance ID for the given region.
     """
-    if pacu_main.fetch_data(['EC2', 'Instances'], 'enum_ec2', '--instances --region ap-northeast-2', True) is False:
+    fields = ["EC2", "Instances"]
+    module = "enum_ec2"
+    args = "--instances --region ap-northeast-2"
+    fetched_ec2_instances = pacu_main.fetch_data(fields, module, args)
+    if fetched_ec2_instances is False:
         return None
-    for instance in session.EC2['Instances']:
-        if instance['Region'] == region:
-            return instance['InstanceId']
+    for instance in session.EC2["Instances"]:
+        if instance["Region"] == region:
+            instance_id = instance["InstanceId"]
+            availability_zone = instance['Placement']['AvailabilityZone']
+            return instance_id, availability_zone
     return None
+
+
+def load_volumes(instance_id, volume_ids):
+    """Loads volumes on an instance.
+
+    Args:
+        instance_id (str): instance_id to attach volumes to
+        volume_ids (list): list of volume_ids to attach to the instance.
+    Returns:
+        bool: True if the volumes were successfully attached.
+    """
+    return instance_id, volume_ids
+
 
 def main(args, pacu_main):
     """Main module function, called from Pacu"""
-
-    ###### Don't modify these. They can be removed if you are not using the function.
     args = parser.parse_args(args)
     session = pacu_main.get_active_session()
-    #input = pacu_main.input
-    #key_info = pacu_main.key_info
-    #get_regions = pacu_main.get_regions
-    ######
-    regions = ['ap-northeast-2']
+    # input = pacu_main.input
+    # key_info = pacu_main.key_info
+    # get_regions = pacu_main.get_regions
+
+    regions = ["ap-northeast-2"]
     for region in regions:
-        instanceId = get_interactive_instance(
+        instance_id, availability_zone = get_interactive_instance(
             pacu_main, session, region
         )
-        if not instanceId:
-            pacu_main.print('Failed to get a valid instanceId')
+        if not instance_id:
+            pacu_main.print("No valid instance found")
             continue
-        pacu_main.print(instanceId)
+        # Set the target instance to load
+        pacu_main.print(instance_id)
+
     summary_data = {}
     return summary_data
 

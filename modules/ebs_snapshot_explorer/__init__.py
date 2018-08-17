27940bc711248460ec7a011b9072be5bf0965816
Alex Morgenstern <alexander.morgenstern@rhinosecuritylabs.com>
2018-08-17 15:51:51 -0700

    Added different methods to attach volumes

--- modules/ebs_snapshot_explorer/__init__.py
+++ modules/ebs_snapshot_explorer/__init__.py
@@ -8,7 +8,7 @@ module_info = {
     'description': 'This module will use an EC2 instance to load Elastic Block Store volumes and snapshots in the account and allow the user to access the data.',
     'services': ['EC2'],
     'prerequisite_modules': [],
-    'arguments_to_autocomplete': ['--regions', '--vols', '--snaps', '--account-ids'],
+    'arguments_to_autocomplete': ['--regions', '--instance'],
 }
 
 parser = argparse.ArgumentParser(add_help=False, description=module_info['description'])
@@ -19,24 +19,10 @@ parser.add_argument(
     help='One or more (comma separated) AWS regions in the format "us-east-1". Defaults to all session regions.'
 )
 parser.add_argument(
-    '--vols',
-    required=False,
-    default=False,
-    action='store_true',
-    help='If this argument is passed without --snaps, this module will only enumerate volumes. If neither are passed, both volumes and snapshots will be enumerated.'
-)
-parser.add_argument(
-    '--snaps',
-    required=False,
-    default=False,
-    action='store_true',
-    help='If this argument is passed without --vols, this module will only enumerate snapshots. If neither are passed, both volumes and snapshots will be enumerated.'
-)
-parser.add_argument(
-    '--account-ids',
+    '--instance',
     required=False,
     default=None,
-    help='One or more (comma separated) AWS account IDs. If snapshot enumeration is enabled, then this module will fetch all snapshots owned by each account in this list of AWS account IDs. Defaults to the current user accounts AWS account ID.'
+    help='Instance to load volumes to in format instance@region(availabilityzone)'
 )
 
 
--- modules/ebs_snapshot_explorer/main.py
+++ modules/ebs_snapshot_explorer/main.py
@@ -43,27 +43,27 @@ def load_volumes(client, print, input, instance_id, volume_ids):
 
     # load volume set
     set_index = 0
-    SET_COUNT = 10
+    SET_COUNT = 3
 
     while set_index < len(volume_ids):
+        current_volume_set = volume_ids[set_index:set_index + SET_COUNT]
         attached = modify_volume_set(
-            client, print, 'attach_volume', instance_id, 
-            volume_ids[set_index:set_index + 40])
+            client, print, 'attach_volume', instance_id, current_volume_set)            
         if not attached:
             print(' Volume attachment failed')
             print(' Exiting...')
             return False
-        input('Press any key to load next set of volumes...')
+        input('    Press enter to load next set of volumes...')
         detached = modify_volume_set(
-            client, print, 'detach_volume', instance_id,
-            volume_ids[set_index:set_index + 40])
+            client, print, 'detach_volume', instance_id, current_volume_set)
         if not detached:
             print(' Volume detachment failed')
             print(' Exiting...')
             return False
+        waiter = client.get_waiter('volume_available')
+        waiter.wait(VolumeIds=current_volume_set)
         set_index += SET_COUNT
     return True
-
 
 def modify_volume_set(client, print, func, instance_id, volume_id_set):
     """Helper function to load volumes on an instance to not overload the
@@ -72,13 +72,12 @@ def modify_volume_set(client, print, func, instance_id, volume_id_set):
     Args:
         client (boto3.client): client to interact with AWS
         print (func): Overwritten built-in print function
-        func (str): Function name to modify_volume_set
+        func (str): Function sname to modify_volume_set
         instance_id (str): instance_id to attach volumes to
-        volume_ids (list): list of volume_ids to attach to the instance.
+        volume_ids (list): list of volume_ids to (de)attach to the instance.
     Returns:
         bool: True if the volumes were successfully attached.
     """
-
     BASE_DEVICE = 'xvd'
     device_offset = 'f'
     for volume_id in volume_id_set:
@@ -92,11 +91,15 @@ def modify_volume_set(client, print, func, instance_id, volume_id_set):
             caller(**kwargs)
             device_offset = chr(ord(device_offset) + 1)
         except ClientError as error:
-            if error.response['Error']['Code'] == 'UnauthorizedOperation':
-                print('  Unauthorized Operation')
+            code = error.response['Error']['Code']
+            if  code == 'UnauthorizedOperation':
+                print('  FAILURE MISSING AWS PERMISSIONS')
+            elif code == 'InvalidAttachment.NotFound':
+                print('  Skipping unattached volume...')
+                continue
             elif error.response['Error']['Code'] == 'VolumeInUse':
-                print('  Volume in Use')
-                print('    Skipping...')
+                print('  Skipping Volume in Use...')
+                continue
             else:
                 print(error)
             return False
@@ -146,11 +149,52 @@ def get_volumes(pacu, session, regions):
             return None
     volume_ids = {}
     for region in regions:
-        volume_ids[region] = []
+        volume_ids[region] = {'available': [], 'in_use': []}
     for volume in ec2_data['Volumes']:
         if volume['Region'] in regions:
-            volume_ids[volume['Region']].append(volume['VolumeId'])
+            if volume['State'] == 'available':
+                volume_ids[region]['available'].append(volume['VolumeId'])
+            elif volume['State'] == 'in-use':
+                volume_ids[region]['in_use'].append(volume['VolumeId'])
     return volume_ids
+
+
+def generate_volumes_from_snapshots(client, snapshots, zone):
+    """ Returns a list of generated volumes"""
+    volume_ids = []
+    for snapshot in snapshots:
+        response = client.create_volume(
+            SnapshotId=snapshot, AvailabilityZone=zone)
+        volume_ids.append(response['VolumeId'])
+    return volume_ids
+
+def generate_snapshots_from_volumes(client, volume_ids):
+    """Returns a list of generated snapshots volumes that are currently in-use
+    """
+    snapshot_ids = []
+    for volume in volume_ids:
+        response = client.create_snapshot(VolumeId=volume)
+        snapshot_ids.append(response['SnapshotId'])
+    return snapshot_ids
+
+def delete_volumes(client, volumes):
+    """Deletes a given list of volumes"""
+    for volume in volumes:
+        try:
+            client.delete_volume(VolumeId=volume)
+        except ClientError as error:
+            raise error
+    return True
+
+
+def delete_snapshots(client, snapshots):
+    """Deletes a given list of snapshots"""
+    for snapshot in snapshots:
+        try:
+            client.delete_snapshot(SnapshotId=snapshot)
+        except ClientError as error:
+            raise error
+    return True
 
 
 def main(args, pacu):
@@ -159,35 +203,42 @@ def main(args, pacu):
     session = pacu.get_active_session()
     print = pacu.print
     input = pacu.input
-    key_info = pacu.key_info
+    #key_info = pacu.key_info
+
     regions = pacu.get_regions('ec2')
-    region = regions[0]
-    print(get_snapshots(pacu, session, regions))
-    print(get_volumes(pacu, session, regions))
-    print(get_interactive_instance(pacu, session, region))
+    snapshots = get_snapshots(pacu, session, regions)
+    volumes = get_volumes(pacu, session, regions)
+    for region in regions:
+        region_snapshots = snapshots[region]
+        client = pacu.get_boto3_client('ec2', region)
+        if args.instance:
+            instance = args.instance.split('@')[0]
+            zone = args.instance.split('@')[1]
+        else:
+            instance, zone = get_interactive_instance(pacu, session, region)
+        print('  Attaching initial volumes...')
+        # Load the pre-existing volumes
+        load_volumes(client, print, input, instance, volumes[region]['available'])
 
-    #client = pacu.get_boto3_client('ec2', region)
+        continue
+        # Generate temporary volumes from running volumes        
+        print('  Attaching in-use volumes...')
+        temp_snaps = generate_snapshots_from_volumes(
+            client, volumes[region]['in_use'])
+        waiter = client.get_waiter('snapshot_completed')
+        waiter.wait(SnapshotIds=temp_snaps)
+        in_use_volumes = generate_volumes_from_snapshots(
+            client, temp_snaps, zone)
+        load_volumes(client, print, input, instance, in_use_volumes)
+        delete_volumes(client, in_use_volumes)
+        delete_snapshots(client, temp_snaps)
 
-
-    #volumes = [
-    #    'vol-0687a3892ffce7f1c',
-    #    'vol-05d1644b7587c3e95',
-    #    'vol-0faddc520225b5b97',
-    #]
-    #instance_id = 'i-0ffc126ebc52e0103'
-    #success = load_volumes(client, print, input, instance_id, volumes)
-    #print(success)
-
-    #for region in regions:
-    #    client = pacu.get_boto3_client('ec2', region)
-    #    instance_id, availability_zone = get_interactive_instance(
-    #        pacu, session, region
-    #    )
-    #    if not instance_id:
-    #        pacu.print("No valid instance found")
-    #        continue
-    #    # Set the target instance to load
-    #    pacu.print(instance_id, availability_zone)
+        # Generate temporary volumes from snapshots
+        print('  Attaching volumes from existing snapshots')
+        temp_volumes = generate_volumes_from_snapshots(
+            client, snapshots[region], zone)
+        load_volumes(client, print, input, instance, temp_volumes)
+        delete_volumes(client, temp_volumes)
 
     summary_data = {}
     return summary_data

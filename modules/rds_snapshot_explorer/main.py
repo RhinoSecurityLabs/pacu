70c068b70e2f4117337b2e8becbb4c5fc9c59cdf
Alexander Morgenstern <alexander.morgenstern@rhinosecuritylabs.com>
2018-09-06 08:34:57 -0700

    Added flow of creating/deleting temp db instances

--- modules/rds_snapshot_explorer/main.py
+++ modules/rds_snapshot_explorer/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import argparse
 
+from botocore.exceptions import ClientError
+
 module_info = {
     'name': 'rds_snapshot_explorer',
     'author': 'Alexander Morgenstern alexander.morgenstern@rhinosecuritylabs.com',
@@ -10,25 +12,105 @@ module_info = {
     'services': ['RDS'],
     'prerequisite_modules': [],
     'external_dependencies': [],
-    'arguments_to_autocomplete': [],
+    'arguments_to_autocomplete': ['--regions'],
 }
 parser = argparse.ArgumentParser(add_help=False, description=module_info['description'])
+parser.add_argument('--regions', required=False, default=None, help='One or more (comma separated) AWS regions in the format us-east-1. Defaults to all session regions.')
+
 
 def main(args, pacu):
     """Main module function, called from Pacu"""
-    # parser.parse_args(args)
-    regions = pacu.get_regions('rds')
+    args = parser.parse_args(args)
+    if args.regions:
+        regions = args.regions.split(',')
+    else:
+        regions = pacu.get_regions('rds')
+
     for region in regions:
         pacu.print('Region: {}'.format(region))
-        all_dbs = []
         client = pacu.get_boto3_client('rds', region)
-        paginator = client.get_paginator('describe_db_instances')
-        instances = paginator.paginate()
-        for instance in instances:
-            all_dbs.extend(instance['DBInstances'])
-        print(all_dbs)
+        active_instances = get_all_region_instances(client)
+        snapshots = create_snapshots_from_instances(client, active_instances)
+        temp_instances = restore_instance_from_snapshots(client, snapshots)
+        pacu.input('Press enter to continue...')
+        delete_instances(client, temp_instances)
+        delete_snapshots(client, snapshots)
     return {}
 
+def delete_instances(client, instances):
+    for instance in instances:
+        waiter = client.get_waiter('db_instance_available')
+        waiter.wait(DBInstanceIdentifier=instance['DBInstanceIdentifier'])
+        try:
+            client.delete_db_instance(
+                DBInstanceIdentifier=instance['DBInstanceIdentifier'],
+                SkipFinalSnapshot=True,
+            )
+        except ClientError as error:
+            print(error)
+            continue
+        waiter = client.get_waiter('db_instance_deleted')
+        waiter.wait(DBInstanceIdentifier=instance['DBInstanceIdentifier'])
+
+def restore_instance_from_snapshots(client, snapshots):
+    identifier = 'temp-instance-'
+    instances = []
+    for snapshot in snapshots:
+        waiter = client.get_waiter('db_snapshot_available')
+        waiter.wait(DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier'])
+        try:
+            response = client.restore_db_instance_from_db_snapshot(
+                DBInstanceIdentifier=identifier + snapshot['DBSnapshotIdentifier'][-1],
+                DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier'],
+            )
+            instances.append(response['DBInstance'])
+        except ClientError as error:
+            print(error)
+            pass
+    return instances
+
+
+def delete_snapshots(client, snapshots):
+    for snapshot in snapshots:
+        waiter = client.get_waiter('db_snapshot_available')
+        waiter.wait(DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier'])
+        try:
+            client.delete_db_snapshot(
+                DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier']
+            )
+        except ClientError as error:
+            print(error)
+            pass
+
+def create_snapshots_from_instances(client, instances):
+    identifier = 'temp-snapshot-'
+    identifier_offset = 0
+    snapshots = []
+    for instance in instances:
+        try:
+            response = client.create_db_snapshot(
+                DBSnapshotIdentifier=identifier + str(identifier_offset),
+                DBInstanceIdentifier=instance['DBInstanceIdentifier'],
+            )
+            snapshots.append(response['DBSnapshot'])
+            identifier_offset += 1
+        except ClientError as error:
+            print(error)         
+            pass
+    return snapshots
+
+
+def get_all_region_instances(client):
+    out = []
+    paginator = client.get_paginator('describe_db_instances')
+    pages = paginator.paginate()
+    try:
+        for page in pages:
+            out.extend(page['DBInstances'])
+        return out
+    except ClientError as error:
+        print(error)
+        return []
 
 def summary(data, pacu_main):
     out = ''

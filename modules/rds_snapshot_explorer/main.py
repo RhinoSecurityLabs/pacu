914870e997e2acca1c894f58d1f4dbfb4faab809
Alexander Morgenstern <alexander.morgenstern@rhinosecuritylabs.com>
2018-09-11 15:15:11 -0700

    Added password prompt

--- modules/rds_snapshot_explorer/main.py
+++ modules/rds_snapshot_explorer/main.py
@@ -16,8 +16,6 @@ module_info = {
 }
 parser = argparse.ArgumentParser(add_help=False, description=module_info['description'])
 parser.add_argument('--regions', required=False, default=None, help='One or more (comma separated) AWS regions in the format us-east-1. Defaults to all session regions.')
-
-
 def main(args, pacu):
     """Main module function, called from Pacu"""
     args = parser.parse_args(args)
@@ -32,10 +30,33 @@ def main(args, pacu):
         active_instances = get_all_region_instances(client)
         snapshots = create_snapshots_from_instances(client, active_instances)
         temp_instances = restore_instance_from_snapshots(client, snapshots)
-        pacu.input('Press enter to continue...')
+        process_instances(pacu, client, temp_instances)
         delete_instances(client, temp_instances)
         delete_snapshots(client, snapshots)
     return {}
+
+def process_instances(pacu, client, instances):
+    for instance in instances:
+        waiter = client.get_waiter('db_instance_available')
+        waiter.wait(DBInstanceIdentifier=instance['DBInstanceIdentifier'])
+        password = pacu.input('  Set Master Password for current Instance: ')
+        if modify_master_password(client, instance, password):
+            pacu.print('  Password Change Successfully')
+        else:
+            pacu.print('  Password Change Failed')
+
+        pacu.input('Press enter to process next instance...')
+
+def modify_master_password(client, instance, password):
+    try:
+        client.modify_db_instance(
+            DBInstanceIdentifier=instance['DBInstanceIdentifier'],
+            MasterUserPassword=password,
+        )
+        return True
+    except ClientError as error:
+        print(error)
+    return False
 
 def delete_instances(client, instances):
     for instance in instances:
@@ -49,24 +70,24 @@ def delete_instances(client, instances):
         except ClientError as error:
             print(error)
             continue
+    #Confirm that all of the instances have been deleted
+    for instance in instances:
         waiter = client.get_waiter('db_instance_deleted')
         waiter.wait(DBInstanceIdentifier=instance['DBInstanceIdentifier'])
 
 def restore_instance_from_snapshots(client, snapshots):
-    identifier = 'temp-instance-'
     instances = []
     for snapshot in snapshots:
         waiter = client.get_waiter('db_snapshot_available')
         waiter.wait(DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier'])
         try:
             response = client.restore_db_instance_from_db_snapshot(
-                DBInstanceIdentifier=identifier + snapshot['DBSnapshotIdentifier'][-1],
+                DBInstanceIdentifier=snapshot['DBSnapshotIdentifier'],
                 DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier'],
             )
             instances.append(response['DBInstance'])
         except ClientError as error:
             print(error)
-            pass
     return instances
 
 
@@ -80,23 +101,20 @@ def delete_snapshots(client, snapshots):
             )
         except ClientError as error:
             print(error)
-            pass
 
 def create_snapshots_from_instances(client, instances):
-    identifier = 'temp-snapshot-'
-    identifier_offset = 0
     snapshots = []
     for instance in instances:
+        waiter = client.get_waiter('db_instance_available')
+        waiter.wait(DBInstanceIdentifier=instance['DBInstanceIdentifier'])
         try:
             response = client.create_db_snapshot(
-                DBSnapshotIdentifier=identifier + str(identifier_offset),
+                DBSnapshotIdentifier=instance['DBInstanceIdentifier'] + '-copy',
                 DBInstanceIdentifier=instance['DBInstanceIdentifier'],
             )
             snapshots.append(response['DBSnapshot'])
-            identifier_offset += 1
         except ClientError as error:
-            print(error)         
-            pass
+            print(error)
     return snapshots
 
 

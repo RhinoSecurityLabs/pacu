4142b56744f7646bbcf84516663669850bf0aa5f
Ryan Gerstenkorn <me@ryanjarv.sh>
2021-01-10 07:45:17 -0600

    Testing, docker and dependency changes.
    * Initial tests
    * Remove awscli dependency, not really a hard dependency on it and
      seems a bit unexpected that we would be managing this for them.
    * Make pacu.py the entrypoint for docker.
      Will behave closer to how it does when running it directly this way.
    * Set lower bounds on boto3/botocore dependencies.
    * Add test Makefile target.
    * Fix github testing

--- Makefile
+++ Makefile
@@ -12,3 +12,13 @@ lint: flake8 mypy
 clean:
 	rm -rf .mypy_cache/
 	rm -rf .pytest_cache/
+	rm -rf out/
+	rm -rf ./**/**/__pycache__/
+
+test:
+	pytest ./tests
+
+graphs:
+	mkdir -p out
+	cd out && pyreverse -S -f ALL -o dot ../pacu.py ../core
+	docker run -it  -v "${PWD}:/dot" -w /dot dotviz  -Tpng out/classes.dot -o out/classes.png
---
+++ tests/test_pacu_session.py
@@ -0,0 +1,50 @@
+import pytest
+from sqlalchemy import orm, create_engine
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import sessionmaker
+
+import settings
+import core.base
+from core.models import PacuSession
+
+# Import base and models after settings are set up
+settings.DATABASE_CONNECTION_PATH = "sqlite:///:memory:"
+
+
+@pytest.fixture(scope='function')
+def db() -> core.base:
+    # Recreate the engine and session maker each run
+    core.base.engine: Engine = create_engine(settings.DATABASE_CONNECTION_PATH)
+    core.base.Session: sessionmaker = sessionmaker(bind=core.base.engine)
+    core.base.Base.metadata.create_all(core.base.engine)
+    yield core.base.Session()
+
+
+def test_sanity(db: orm.session.Session):
+    assert PacuSession().__class__ == PacuSession
+
+
+@pytest.fixture(scope='function')
+def pacu_session(db: orm.session.Session):
+    query: orm.Query = db.query(PacuSession)
+    assert query.count() == 0
+
+    pacu_session = PacuSession()
+    db.add(pacu_session)
+    yield pacu_session
+
+
+def test_pacu_session_in_db(db, pacu_session: PacuSession):
+    query: orm.Query = db.query(PacuSession)
+    result: PacuSession = query.first()
+    assert result.id == pacu_session.id
+
+
+@pytest.fixture(scope='function')
+def active_session(db, pacu_session: PacuSession):
+    pacu_session.activate(db)
+    yield pacu_session
+
+
+def test_active_session(active_session: PacuSession):
+    assert active_session.is_active

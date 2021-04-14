88b113f25f61096babf9201117a2d541fc880e0b
Manas Bellani <manasmbellani@gmail.com>
2021-04-15 03:29:43 +1000

    Added new module guardduty__list_findings, and fixed bug in detection__enum_services when enum GuardDuty (#193)
    * -
    
    * Revert "-"
    
    This reverts commit 6e3949d5df5b19e622a55aa899912df6c070a318.
    
    * Added a new module to print Guardduty findings and stats, Fixed bug in enumeration for Guardduty detector
    
    * Added a new module that uses detection__enum_services module to determine all detectors for GuardDuty that are enabled and pulls the statstics and all findings for the specific detector.
    
    * Fixed a bug in pre-req module detection__enum_services which was failing when enumerating AWS fips region by adding error handling to ensure that  module continues to run and writes data to back-end database
    
    * modified code to write guardduty findings to outfile, instead of stdin
    
    * feature_guardduty_list_findings/main.py formatting
    
    Minor formatting change.
    
    * change output dir guardduty__list_findings
    
    This moves the output directory to sessions/<session>/guardduty
    
    Co-authored-by: Manas Bellani <edwards.aus@hotmail.com>
    Co-authored-by: Ryan Gerstenkorn <ryan.gerstenkorn@rhinosecuritylabs.com>

--- modules/detection__enum_services/main.py
+++ modules/detection__enum_services/main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 from copy import deepcopy
-from botocore.exceptions import ClientError,EndpointConnectionError
+from botocore.exceptions import ClientError, EndpointConnectionError
 
 
 module_info = {

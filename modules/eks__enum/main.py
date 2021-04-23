cf0486241054b726f1431cb6df13159cf0a20642
crtl_freq <crtl_freq@pop-os.localdomain>
2021-04-22 19:52:44 -0500

    EKS data is being written to the database for clusters that are found. fips endpoints are being ignored.

--- modules/eks__enum/main.py
+++ modules/eks__enum/main.py
@@ -21,15 +21,22 @@ module_info = {
 parser = argparse.ArgumentParser(add_help=False, description=module_info['description'])
 parser.add_argument('--regions', required=False, default=None, help='One or more (comma separated) AWS regions in the format "us-east-1". Defaults to all session regions.')
 
+# fips endpoints cause duplicates in the data that we pull back, and I see no reason atm to include them
+def remove_fips(original_regions):
+    non_fips_regions = []
+    for region in original_regions:
+        if "fips" not in region:
+            non_fips_regions.append(region)
+    return non_fips_regions
+
+
 def main(args, pacu_main):
     pacu_main.update_regions() # apparently we can't trust pacu to run this on boot, seems odd. 
     args = parser.parse_args(args)
     print = pacu_main.print
     session = pacu_main.get_active_session()
     get_regions = pacu_main.get_regions
-    data = {
-        "clusters": []
-    }
+    data = {}
 
     if args.regions is None:
         regions = get_regions('eks')
@@ -38,13 +45,17 @@ def main(args, pacu_main):
             return
     else:
         regions = args.regions.split(',')
-    
+
+    regions = remove_fips(regions)
     for region in regions:
         eks_client = pacu_main.get_boto3_client('eks', region)
         clusters = eks_client.list_clusters()["clusters"]
         print(f"clusters in {region}: {clusters}")
         if len(clusters) > 0:
-            data['clusters'].append(clusters)
+            data[region] = {
+            "clusters": []
+            }
+            data[region]['clusters'].append(clusters)
         
     session.update(pacu_main.database, EKS=data) 
     return "All good brother!"

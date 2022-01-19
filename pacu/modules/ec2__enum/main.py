6b795b6db7d217b194bc38d0f0af2f925759e039
Gregory Fuentes <gregoryfuentes80@gmail.com>
2022-01-19 09:47:12 -0600

    Debug other data types

--- pacu/modules/ec2__enum/main.py
+++ pacu/modules/ec2__enum/main.py
@@ -174,6 +174,8 @@ def main(args, pacu_main):
                     next_token = response['NextToken']
                 for reservation in response['Reservations']:
                     for instance in reservation['Instances']:
+                        if type(instance) == str:
+                            print("instance type is a string")
                         instance['Region'] = region
                         instances.append(instance)
                         # Scan tags for secrets

5927ca9b6b9c7f0c35869a45ff711943db12a58b
DaveYesland <david.yesland@rhinosecuritylabs.com>
2023-06-12 14:50:51 -0700

    Expand route53__enum enumeration

--- pacu/modules/route53__enum/main.py
+++ pacu/modules/route53__enum/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import argparse
 from botocore.exceptions import ClientError
-import json
 
 module_info = {
     # Name of the module (should be the same as the filename).
@@ -41,8 +40,10 @@ parser = argparse.ArgumentParser(add_help=False, description=module_info['descri
 
 
 def get_hosted_zones(client):
-    hosted_zones = client.list_hosted_zones()['HostedZones']
-
+    hosted_zones = []
+    paginator = client.get_paginator('list_hosted_zones')
+    for hosted_zone in paginator.paginate():
+        hosted_zones += hosted_zone['HostedZones']
     zones = {}
 
     if len(hosted_zones) > 0:
@@ -54,6 +55,23 @@ def get_hosted_zones(client):
         print("No HostedZones found")
 
     return zones
+
+
+def get_resource_record_sets_for_zone_id(client, hosted_zone_id):
+    record_sets = {}
+    all_records_for_zone = []
+    paginator = client.get_paginator('list_resource_record_sets')
+    for resource_records in paginator.paginate(HostedZoneId=hosted_zone_id):
+        all_records_for_zone += resource_records['ResourceRecordSets']
+    record_sets[hosted_zone_id] = {'ResourceRecordSets': all_records_for_zone}
+    if len(record_sets[hosted_zone_id]) > 0:
+        print(f"ResourceRecordSets for {hosted_zone_id}:")
+        for record in record_sets[hosted_zone_id]["ResourceRecordSets"]:
+            print(f"Name: {record['Name']} Type: {record['Type']}")
+    else:
+        print("No ResourceRecordSets found")
+
+    return record_sets
 
 
 def get_query_logging_config(client):
@@ -69,11 +87,13 @@ def get_query_logging_config(client):
     return configs
 
 
-def zones_plus_config(zones, configs):
+def zones_plus_config_and_records(zones, configs, records):
     for con in configs:
         if con['HostedZoneId'] in zones.keys():
             zones[con['HostedZoneId']].update({'CloudWatchLogsLogGroupArn': con['CloudWatchLogsLogGroupArn']})
             zones[con['HostedZoneId']].update({'QueryLoggingConfigId': con['Id']})
+    for zone_id in records.keys():
+        zones[zone_id].update(records[zone_id])
 
     return zones
 
@@ -102,7 +122,16 @@ def main(args, pacu_main):
         print(f'Failed to list R53 Hosted Zone Query Logging Configurations: {error}')
         return
 
-    data = zones_plus_config(zones=zones, configs=confs)
+    records = {}
+    for hosted_zone_id in zones.keys():
+        try:
+            records_for_zone = get_resource_record_sets_for_zone_id(client=client, hosted_zone_id=hosted_zone_id)
+        except ClientError as error:
+            print(f'Failed to list R53 Resource Record Sets: {error}')
+            continue
+        records.update(records_for_zone)
+
+    data = zones_plus_config_and_records(zones=zones, configs=confs, records=records)
 
     session.update(pacu_main.database, Route53=data)
 
@@ -111,6 +140,10 @@ def main(args, pacu_main):
 
 def summary(data, pacu_main):
     if len(data) > 0:
-        return f"Found {len(data)} hosted zones:\n{json.dumps(data, indent=2)}"
+        hosted_zone_count = len(data)
+        total_records = 0
+        for zone_id in data.keys():
+            total_records += data[zone_id]['ResourceRecordSetCount']
+        return f"Found {hosted_zone_count} hosted zones.\nFound {total_records} resource records."
     else:
         return 'No hosted zones found.'

dba59f40db007fa29d9e08d68d22b7f4013e5710
6a6f656c <outn2015@gmail.com>
2024-05-24 12:07:16 -0400

    added regions

--- pacu/modules/mq__enum/main.py
+++ pacu/modules/mq__enum/main.py
@@ -24,63 +24,79 @@ module_info = {
 
 parser = argparse.ArgumentParser(add_help=False, description=module_info["description"])
 
+parser.add_argument('--regions', required=False, default=None, help=strip_lines('''
+    One or more (comma separated) AWS regions in the format "us-east-1". Defaults to all session regions.
+'''))
 
 def main(args, pacu_main: "Main"):
     session = pacu_main.get_active_session()
 
     # Don't modify these. They can be removed if you are not using the function.
-    # args = parser.parse_args(args)
+    args = parser.parse_args(args)
     print = pacu_main.print
 
     key_info = pacu_main.key_info
     fetch_data = pacu_main.fetch_data
 
     # End don't modify
-
-    client = pacu_main.get_boto3_client("mq")
-
-    # Prepare output file to store ECR data
-    now = time.time()
-    outfile_path = str(downloads_dir() / f"mq_enum_{now}.json")
-
-    try:
-        response = client.list_brokers(
-            MaxResults=10_000,
-        )
-
-    except Exception as error:
-        print(
-            "Unable to list brokers; Check credentials or No brokers are available. Error: {}".format(
-                error
-            )
-        )
-        return
+    get_regions = pacu_main.get_regions
+    if not args.regions:
+        regions = get_regions('mq')
+    else:
+        regions = args.regions.split(',')
 
     summary_data = {}
     summary_data["mq"] = {}
 
-    for broker in response["BrokerSummaries"]:
-        broker_details = client.describe_broker(BrokerId=broker["BrokerId"])
-        summary_data["mq"][broker["BrokerId"]] = {}
-        summary_data["mq"][broker["BrokerId"]]["AuthenticationStrategy"] = (
-            broker_details["AuthenticationStrategy"]
-        )
-        summary_data["mq"][broker["BrokerId"]]["PubliclyAccessible"] = broker_details[
-            "PubliclyAccessible"
-        ]
-        summary_data["mq"][broker["BrokerId"]]["BrokerName"] = broker_details[
-            "BrokerName"
-        ]
-        summary_data["mq"][broker["BrokerId"]]["BrokerState"] = broker_details[
-            "BrokerState"
-        ]
-        summary_data["mq"][broker["BrokerId"]]["Users"] = broker_details["Users"]
-        summary_data["mq"][broker["BrokerId"]]["EngineType"] = broker_details[
-            "EngineType"
-        ]
-        summary_data["mq"][broker["BrokerId"]]["ConsoleURL"] = [
-            url["ConsoleURL"] for url in broker_details["BrokerInstances"]
-        ]
+    for region in regions:
+        print('Starting region {}...'.format(region))
+        summary_data["mq"][region] = {}
+
+        try:
+            client = pacu_main.get_boto3_client("mq", region)
+        except Exception as error:
+            print("Unable to connect to MQ service. Error: {}".format(error))
+            continue
+
+        # Prepare output file to store ECR data
+        now = time.time()
+        outfile_path = str(downloads_dir() / f"mq_enum_{now}.json")
+
+        try:
+            response = client.list_brokers(
+                MaxResults=100,
+            )
+
+        except Exception as error:
+            print(
+                "Unable to list brokers; Check credentials or No brokers are available. Error: {}".format(
+                    error
+                )
+            )
+            continue
+        print("Found {} brokers".format(len(response["BrokerSummaries"])))
+        for broker in response["BrokerSummaries"]:
+            broker_details = client.describe_broker(BrokerId=broker["BrokerId"])
+            summary_data["mq"][region][broker["BrokerId"]] = {}
+            summary_data["mq"][region][broker["BrokerId"]]["AuthenticationStrategy"] = (
+                broker_details["AuthenticationStrategy"]
+            )
+            summary_data["mq"][region][broker["BrokerId"]]["PubliclyAccessible"] = broker_details[
+                "PubliclyAccessible"
+            ]
+            summary_data["mq"][region][broker["BrokerId"]]["BrokerName"] = broker_details[
+                "BrokerName"
+            ]
+            summary_data["mq"][region][broker["BrokerId"]]["BrokerState"] = broker_details[
+                "BrokerState"
+            ]
+            summary_data["mq"][region][broker["BrokerId"]]["Users"] = broker_details["Users"]
+            summary_data["mq"][region][broker["BrokerId"]]["EngineType"] = broker_details[
+                "EngineType"
+            ]
+            summary_data["mq"][region][broker["BrokerId"]]["ConsoleURL"] = [
+                url["ConsoleURL"] for url in broker_details["BrokerInstances"]
+            ]
 
     # Write all the data to the output file
     print("Writing all MQ results to file: {}".format(outfile_path))
@@ -89,12 +105,15 @@ def main(args, pacu_main: "Main"):
 
     return summary_data
 
-
 def summary(data, pacu_main):
     out = ""
-    out += "Num of MQ brokers found: {} \n".format(len(data["mq"]))
+ 
     total_users = 0
-    for broker in data["mq"]:
-        total_users += len(broker["Users"])
+    total_brokers = 0
+    for region in data["mq"]:
+        total_brokers += len(data["mq"][region])
+        for broker in data["mq"][region]:
+            total_users += len(data["mq"][region][broker]["Users"])
+    out += "Num of MQ brokers found: {} \n".format(total_brokers)
     out += "Num of MQ users found: {} \n".format(total_users)
     return out

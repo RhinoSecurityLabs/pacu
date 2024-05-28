e7f3b71aabe0e06808856ebe8bf34395f26a0504
h00die <h00die@users.noreply.github.com>
2024-05-28 13:14:30 -0400

    add --all flag to sns__subscribe
    allow user to submit multiple sns topics, or auto enumerate them and run through them all

--- pacu/modules/sns__subscribe/main.py
+++ pacu/modules/sns__subscribe/main.py
@@ -17,19 +17,29 @@ module_info = {
         """
     ),
     "services": ["SNS"],
-    "prerequisite_modules": [],
+    "prerequisite_modules": ['sns__enum'],
     "arguments_to_autocomplete": [],
 }
 
 parser = argparse.ArgumentParser(add_help=False, description=module_info["description"])
 
 parser.add_argument(
-    "--topic",
-    required=True,
+    "--all",
+    action='store_true',
+    help=strip_lines(
+        """
+    Attempt to subscribe to all Topic(s).
+    """
+    ),
+)
+
+parser.add_argument(
+    "--topics",
+    required=False,
     default=False,
     help=strip_lines(
         """
-    Topic ARN to subscribe to. Typically in the format arn:aws:sns:<region>:<client_id>:<topic_name>.
+    Specific Topic(s) ARN to subscribe to. Comma separated Typically in the format arn:aws:sns:<region>:<client_id>:<topic_name>.
     """
     ),
 )
@@ -63,54 +73,75 @@ def main(args, pacu_main: "Main"):
         print("  Please use a valid email address for email parameter\n")
         return
 
-    if not args.topic.startswith("arn:aws:sns:"):
-        print(
-            "  Please use a valid topic, typical format is arn:aws:sns:<region>:<client_id>:<topic_name>\n"
-        )
-        return
-
-    if args.topic.endswith(".fifo"):
-        print(
-            "  Please use a valid topic, fifo topics can't be subscribed to via email\n"
-        )
-        return
-
-    region_from_arn = args.topic.split(":")[3]
-    client = pacu_main.get_boto3_client("sns", region=region_from_arn)
-
-    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sns/client/subscribe.html
-    try:
-        response = client.subscribe(
-            TopicArn=args.topic,
-            Protocol="email",
-            Endpoint=args.email,
-            ReturnSubscriptionArn=True,
-        )
-        print(
-            "Subscribed successfully, check email for subscription confirmation. Confirmation ARN: {}".format(
-                response["SubscriptionArn"]
-            )
-        )
-
-        new_data = {
-            "Protocol": "email",
-            "Endpoint": args.email,
-            "SubscriptionArn": response["SubscriptionArn"],
-        }
-
-        sns_data = deepcopy(session.SNS)
-        if "sns" not in sns_data:
-            sns_data["sns"] = {}
-        if not region_from_arn in sns_data["sns"]:
-            sns_data["sns"][region_from_arn] = {}
-        if not args.topic in sns_data["sns"][region_from_arn]:
-            sns_data["sns"][region_from_arn][args.topic] = {}
-        if not "Subscribers" in sns_data["sns"][region_from_arn][args.topic]:
-            sns_data["sns"][region_from_arn][args.topic]["Subscribers"] = [new_data]
+    if args.topics:
+        topics_list = ",".join([topic.strip() for topic in args.topics.split(",")])
+    elif args.all:
+        if fetch_data(['SNS'], 'sns__enum', '') is False:
+            print('No topics enumerated...')
+            return
         else:
-            sns_data["sns"][region_from_arn][args.topic]["Subscribers"].append(new_data)
-        session.update(pacu_main.database, SNS=sns_data)
-    except Exception as error:
-        print(
-            "Unable to subscribe, check permissions and topic. Error: {}".format(error)
-        )
+            topics_list = []
+            for region in session.SNS['sns']:
+                for topic in session.SNS['sns'][region]:
+                    if not topic.endswith(".fifo"):
+                        topics_list.append(topic)
+            topics_list = ",".join(topics_list)
+    else:
+        print('--topics <topics> or --all required')
+        return
+    
+    for topic in topics_list.split(","):
+        topic = topic.strip()
+        if not topic.startswith("arn:aws:sns:"):
+            print(
+                "  Please use a valid topic, typical format is arn:aws:sns:<region>:<client_id>:<topic_name>. Invalid item: {}\n".format(topic)
+            )
+            return
+
+        if topic.endswith(".fifo"):
+            print(
+                "  Please use a valid topic, fifo topics can't be subscribed to via email. Invalid item: {}\n".format(topic)
+            )
+            return
+
+    for topic in topics_list.split(","):
+        topic = topic.strip()
+        region_from_arn = topic.split(":")[3]
+        client = pacu_main.get_boto3_client("sns", region=region_from_arn)
+    
+        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sns/client/subscribe.html
+        try:
+            response = client.subscribe(
+                TopicArn=topic,
+                Protocol="email",
+                Endpoint=args.email,
+                ReturnSubscriptionArn=True,
+            )
+            print(
+                "Subscribed successfully, check email for subscription confirmation. Confirmation ARN: {}".format(
+                    response["SubscriptionArn"]
+                )
+            )
+
+            new_data = {
+                "Protocol": "email",
+                "Endpoint": args.email,
+                "SubscriptionArn": response["SubscriptionArn"],
+            }
+
+            sns_data = deepcopy(session.SNS)
+            if "sns" not in sns_data:
+                sns_data["sns"] = {}
+            if not region_from_arn in sns_data["sns"]:
+                sns_data["sns"][region_from_arn] = {}
+            if not topic in sns_data["sns"][region_from_arn]:
+                sns_data["sns"][region_from_arn][topic] = {}
+            if not "Subscribers" in sns_data["sns"][region_from_arn][topic]:
+                sns_data["sns"][region_from_arn][topic]["Subscribers"] = [new_data]
+            else:
+                sns_data["sns"][region_from_arn][topic]["Subscribers"].append(new_data)
+            session.update(pacu_main.database, SNS=sns_data)
+        except Exception as error:
+            print(
+                "Unable to subscribe, check permissions and topic. Error: {}".format(error)
+            )

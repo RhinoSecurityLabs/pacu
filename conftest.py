8798c6a229e5b65518c646cf3670ab1424c072d6
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-05-30 22:04:58 +0200

    update aws_credentials fixture with cleanup

--- conftest.py
+++ conftest.py
@@ -62,7 +62,15 @@ def pacu_with_data(pacu: Main, active_session: PacuSession):
 @pytest.fixture
 def aws_credentials():
     """Mocked AWS Credentials for moto."""
+
+    original_env = dict(os.environ)
+
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
+
+    yield
+
+    os.environ.clear()
+    os.environ.update(original_env)

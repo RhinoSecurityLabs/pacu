5ffe24110f98686c91118996dc38ca14df484494
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-05-31 22:26:21 +0200

    add conftest

---
+++ pacu/modules/cognito__attack/tests/conftest.py
@@ -0,0 +1,61 @@
+import pytest
+import moto
+import boto3
+from pacu.settings import REGION
+
+from pacu.modules.cognito__attack.tests.dataclasses import CognitoCredentials
+
+
+@pytest.fixture
+def mock_cognito_user_pool():
+
+    with moto.mock_cognitoidp():
+
+        client = boto3.client(
+            "cognito-idp",
+            region_name=REGION,
+        )
+
+        response = client.create_user_pool(
+            PoolName="TestUserPool",
+            UsernameAttributes=[
+                "email",
+            ],
+            # Schema=[
+            #     {
+            #         "AttributeName": "email",
+            #         "AttributeDataType": "String",
+            #     }
+            # ],
+        )
+
+        user_pool_id = response["UserPool"]["Id"]
+
+        client_response = client.create_user_pool_client(
+            ClientName="AppClient",
+            GenerateSecret=False,
+            UserPoolId=user_pool_id,
+        )
+
+        cog = CognitoCredentials(
+            client=client,
+            user_pool_id=user_pool_id,
+            client_id=client_response["UserPoolClient"]["ClientId"],
+            client_name=client_response["UserPoolClient"]["ClientName"],
+            identity_pool_id=None,
+        )
+
+    with moto.mock_cognitoidentity():
+        c = boto3.client(
+            "cognito-identity",
+            region_name=REGION,
+        )
+
+        c_resposnse = c.create_identity_pool(
+            IdentityPoolName="TestIdentityPool",
+            AllowUnauthenticatedIdentities=False,
+        )
+
+        cog.identity_pool_id = c_resposnse["IdentityPoolId"]
+
+    yield cog

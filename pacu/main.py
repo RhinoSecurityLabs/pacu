fa3bea4dff5afb6b9c78d20f4a323a2617a94273
h00die <michaeltcyr@gmail.com>
2024-06-04 19:26:28 -0400

    remove unused variable

--- pacu/main.py
+++ pacu/main.py
@@ -17,7 +17,7 @@ from pathlib import Path
 from typing import List, Optional, Any, Dict, Union, Tuple
 
 from pacu.core import lib
-from pacu.core.lib import session_dir, home_dir
+from pacu.core.lib import session_dir
 
 try:
     import jq  # type: ignore

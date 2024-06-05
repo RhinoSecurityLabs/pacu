1dca34cdb0fe673023f053a629e7f7027d440c5b
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-06-05 06:23:56 +0200

    add minimal test

--- pacu/modules/cognito__attack/tests/test_cognito__attack.py
+++ pacu/modules/cognito__attack/tests/test_cognito__attack.py
@@ -1,93 +1,41 @@
+import builtins
 import unittest.mock
 
-import boto3
-import moto
-from pacu import settings
 from pacu.main import Main
 from pacu.modules.cognito__attack.main import main
-from pacu.modules.cognito__attack.tests.dataclasses import (
-    AWSCredentials,
-    CognitoCredentials,
-)
-import builtins
-
+from pacu.modules.cognito__attack.tests.dataclasses import CognitoServiceConfig
 
 EMAIL = "test@example.com"
 USERNAME = "random"
 PASSWORD = "XXXXXXXXXXXXX"
 
 
-def test_me(
-    pacu: Main,
+def test_cognito__attack_minimal(
+    pacu: Main, mock_cognito_user_pool: CognitoServiceConfig
 ):
 
-    with moto.mock_cognitoidp():
+    args = [
+        "--username",
+        EMAIL,
+        "--email",
+        EMAIL,
+        "--password",
+        PASSWORD,
+        "--identity_pools",
+        mock_cognito_user_pool.identity_pool_id,
+        "--user_pool_clients",
+        f"{mock_cognito_user_pool.client_id}@{mock_cognito_user_pool.user_pool_id}",
+    ]
 
-        client = boto3.client(
-            "cognito-idp",
-            region_name=settings.REGION,
-        )
+    input_list = ["n", "", "n", "n", "n"]
 
-        response = client.create_user_pool(
-            PoolName="TestUserPool",
-            UsernameAttributes=[
-                "email",
-            ],
-            # Schema=[
-            #     {
-            #         "AttributeName": "email",
-            #         "AttributeDataType": "String",
-            #     }
-            # ],
-        )
+    with unittest.mock.patch.object(builtins, "input", side_effect=input_list):
+        main(args=args, pacu_main=pacu)
 
-        user_pool_id = response["UserPool"]["Id"]
+    response = mock_cognito_user_pool.client.admin_get_user(
+        UserPoolId=mock_cognito_user_pool.user_pool_id, Username=EMAIL
+    )
 
-        client_response = client.create_user_pool_client(
-            ClientName="AppClient",
-            GenerateSecret=False,
-            UserPoolId=user_pool_id,
-        )
+    user_status = response["UserStatus"]
 
-        cog = CognitoCredentials(
-            client=client,
-            user_pool_id=user_pool_id,
-            client_id=client_response["UserPoolClient"]["ClientId"],
-            client_name=client_response["UserPoolClient"]["ClientName"],
-            identity_pool_id=None,
-        )
-
-        with moto.mock_cognitoidentity():
-            c = boto3.client(
-                "cognito-identity",
-                region_name=settings.REGION,
-            )
-
-            c_resposnse = c.create_identity_pool(
-                IdentityPoolName="TestIdentityPool",
-                AllowUnauthenticatedIdentities=False,
-            )
-
-            cog.identity_pool_id = c_resposnse["IdentityPoolId"]
-
-        args = [
-            "--username",
-            EMAIL,
-            "--email",
-            EMAIL,
-            "--password",
-            PASSWORD,
-            "--identity_pools",
-            cog.identity_pool_id,
-            "--user_pool_clients",
-            f"{cog.client_id}@{cog.user_pool_id}",
-        ]
-
-        with unittest.mock.patch.object(
-            builtins, "input", side_effect=["n", "", "", "", ""]
-        ):
-            main(args=args, pacu_main=pacu)
-
-        response = client.admin_get_user(UserPoolId=cog.user_pool_id, Username=EMAIL)
-
-        user_status = response["UserStatus"]
+    assert user_status == "CONFIRMED"

ebb81b0429d838fa656a88c26378bcb39ac186f6
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-06-08 19:52:50 +0200

    add sanity test

--- pacu/modules/cognito__attack/tests/test_cognito__attack.py
+++ pacu/modules/cognito__attack/tests/test_cognito__attack.py
@@ -39,3 +39,41 @@ def test_cognito__attack_minimal(
     user_status = response["UserStatus"]
 
     assert user_status == "CONFIRMED"
+
+def test_sanity(pacu: Main, mock_cognito_user_pool: CognitoServiceConfig):
+    conn = pacu.get_boto3_client("cognito-idp", REGION)
+    pool_id = mock_cognito_user_pool.user_pool_id
+    client_id = mock_cognito_user_pool.client_id
+
+    conn.admin_create_user(
+        UserPoolId=pool_id,
+        Username=USERNAME,
+        TemporaryPassword=PASSWORD,
+    )
+
+    key = bytes(str(PASSWORD).encode("latin-1"))
+    msg = bytes(str(USERNAME + client_id).encode("latin-1"))
+    new_digest = hmac.new(key, msg, hashlib.sha256).digest()
+    secret_hash = base64.b64encode(new_digest).decode()
+    result = conn.initiate_auth(
+        ClientId=client_id,
+        AuthFlow="USER_SRP_AUTH",
+        AuthParameters={
+            "USERNAME": USERNAME,
+            "SRP_A": uuid.uuid4().hex,
+            "SECRET_HASH": secret_hash,
+        },
+    )
+
+    result = conn.respond_to_auth_challenge(
+        ClientId=client_id,
+        ChallengeName=result["ChallengeName"],
+        ChallengeResponses={
+            "PASSWORD_CLAIM_SIGNATURE": str(uuid.uuid4()),
+            "PASSWORD_CLAIM_SECRET_BLOCK": result["Session"],
+            "TIMESTAMP": str(uuid.uuid4()),
+            "USERNAME": USERNAME,
+        },
+    )
+
+    assert result["ResponseMetadata"]["HTTPStatusCode"] == 200

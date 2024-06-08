440b0e196b5c6e09e63d8d4969a91165fbeabab7
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-06-08 20:30:02 +0200

    update tests

--- pacu/modules/cognito__attack/tests/test_cognito__attack.py
+++ pacu/modules/cognito__attack/tests/test_cognito__attack.py
@@ -32,9 +32,15 @@ def test_cognito__attack_minimal(
         f"{mock_cognito_user_pool.client_id}@{mock_cognito_user_pool.user_pool_id}",
     ]
 
-    input_list = ["n", "", "n", "n", "n"]
+    user_input_list = [
+        "random_token",  # Enter verification code for user
+        "",  # Enter attribute name to modify for user
+        "",  # Enter attribute value to set for user
+        "",  # Modify more custom attributes? (y/n)
+        "n",  # Enter the number of the role you want to assume (or "n" to skip):
+    ]
 
-    with unittest.mock.patch.object(builtins, "input", side_effect=input_list):
+    with unittest.mock.patch.object(builtins, "input", side_effect=user_input_list):
         main(args=args, pacu_main=pacu)
 
     response = mock_cognito_user_pool.client.admin_get_user(
@@ -44,27 +50,32 @@ def test_cognito__attack_minimal(
     user_status = response["UserStatus"]
 
     assert user_status == "CONFIRMED"
+    assert response["UserAttributes"][0]["Value"] == EMAIL
+    assert response["Username"] == response["UserAttributes"][1]["Value"]
 
-def test_sanity(pacu: Main, mock_cognito_user_pool: CognitoServiceConfig):
+
+def test_cognito__attack_sanity(
+    pacu: Main, mock_cognito_user_pool: CognitoServiceConfig
+):
     conn = pacu.get_boto3_client("cognito-idp", REGION)
     pool_id = mock_cognito_user_pool.user_pool_id
     client_id = mock_cognito_user_pool.client_id
 
     conn.admin_create_user(
         UserPoolId=pool_id,
-        Username=USERNAME,
+        Username=EMAIL,
         TemporaryPassword=PASSWORD,
     )
 
     key = bytes(str(PASSWORD).encode("latin-1"))
-    msg = bytes(str(USERNAME + client_id).encode("latin-1"))
+    msg = bytes(str(EMAIL + client_id).encode("latin-1"))
     new_digest = hmac.new(key, msg, hashlib.sha256).digest()
     secret_hash = base64.b64encode(new_digest).decode()
     result = conn.initiate_auth(
         ClientId=client_id,
         AuthFlow="USER_SRP_AUTH",
         AuthParameters={
-            "USERNAME": USERNAME,
+            "USERNAME": EMAIL,
             "SRP_A": uuid.uuid4().hex,
             "SECRET_HASH": secret_hash,
         },
@@ -77,7 +88,7 @@ def test_sanity(pacu: Main, mock_cognito_user_pool: CognitoServiceConfig):
             "PASSWORD_CLAIM_SIGNATURE": str(uuid.uuid4()),
             "PASSWORD_CLAIM_SECRET_BLOCK": result["Session"],
             "TIMESTAMP": str(uuid.uuid4()),
-            "USERNAME": USERNAME,
+            "USERNAME": EMAIL,
         },
     )
 
